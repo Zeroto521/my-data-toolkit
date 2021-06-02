c7269b76bb9d36c3d0b7623d3dd7ccf3c264d803
Zero <Zeroto521@gmail.com>
2021-06-02 10:50:01 +0800

    TYP: create a type called `Pd` to represent `pd.DataFrame` and `pd.Series`

---
+++ dtoolkit/_typing.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from typing import TypeVar, Union
+
+from pandas import DataFrame, Series
+
+PandasType = Union[Series, DataFrame]
+Pd = TypeVar("Pd", bound=PandasType)
