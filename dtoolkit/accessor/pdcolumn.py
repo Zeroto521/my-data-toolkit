1a6df349ad9279f0bbe68fa4975c900627cc7e1e
Zero <Zeroto521@gmail.com>
2021-06-02 10:53:24 +0800

    ENH: let `Pd.col.columns` could show the columns name

---
+++ dtoolkit/accessor/pdcolumn.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+from dtoolkit._typing import Pd
+
+
+@pd.api.extensions.register_dataframe_accessor("col")
+@pd.api.extensions.register_series_accessor("col")
+class ColumnAccessor:
+
+    def __init__(self, obj: Pd):
+        self._obj = obj
+
+    @property
+    def columns(self):
+        if isinstance(self._obj, pd.Series):
+            return self._obj.name
+        else:
+            return self._obj.columns
