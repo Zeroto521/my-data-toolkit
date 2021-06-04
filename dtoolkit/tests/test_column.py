596e737b5656e3d1ae71904f7cf4100079896b26
Zero <Zeroto521@gmail.com>
2021-06-04 09:36:40 +0800

    TST: test the column accessor

---
+++ dtoolkit/tests/test_column.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from dtoolkit.accessor import PandasColumnAccessor
+
+
+def test_series_column():
+    column = "item"
+    s = pd.Series(range(10), name=column)
+    assert s.col.columns == column
+
+
+def test_dataframe_column():
+    data = {"a": range(10), "b": range(10)}
+    d = pd.DataFrame(data)
+    assert list(d.col.columns) == list(data.keys())
