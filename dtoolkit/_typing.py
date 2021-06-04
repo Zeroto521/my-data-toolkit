5e8663477d11185fdbba020eb368be6a04cfc123
Zero <Zeroto521@gmail.com>
2021-06-04 09:19:38 +0800

    TYP: add function type

--- dtoolkit/_typing.py
+++ dtoolkit/_typing.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
-from typing import TypeVar, Union
+from typing import Any, Callable, TypeVar, Union
 
 from pandas import DataFrame, Series
 
 PandasType = Union[Series, DataFrame]
 Pd = TypeVar("Pd", bound=PandasType)
+
+FuncType = Callable[..., Any]
+F = TypeVar("F", bound=FuncType)
