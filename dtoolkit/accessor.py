ae77bfa76596831ea38aad615c98d06f913b1ce1
Zero <Zeroto521@gmail.com>
2021-06-04 09:36:24 +0800

    TYP: add `columns` return's typing

--- dtoolkit/accessor.py
+++ dtoolkit/accessor.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import pandas as pd
 
 from ._typing import Pd
@@ -6,12 +8,11 @@ from ._typing import Pd
 @pd.api.extensions.register_dataframe_accessor("col")
 @pd.api.extensions.register_series_accessor("col")
 class PandasColumnAccessor:
-
     def __init__(self, obj: Pd):
         self._obj = obj
 
     @property
-    def columns(self):
+    def columns(self) -> str | pd.core.indexes.base.Index:
         if isinstance(self._obj, pd.Series):
             return self._obj.name
         else:
