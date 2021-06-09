586b5a62320befa0ef91e39b8da4c6c8ee1f7eef
Zero <Zeroto521@gmail.com>
2021-06-09 16:59:53 +0800

    TST: finish buffer test function

--- dtoolkit/tests/test_buffer.py
+++ dtoolkit/tests/test_buffer.py
@@ -1,23 +1,75 @@
 import geopandas as gpd
+import numpy as np
+import pandas as pd
 import pandas._testing as tm
-from dtoolkit.buffer import geographic_buffer
-
+import pytest
+from dtoolkit.buffer import _geographic_buffer, geographic_buffer, string_or_int_to_crs
+from pyproj import CRS
+from shapely import wkt
 
 my_wkts = ["Point(120 50)", "Point(150 -30)", "Point(100 1)"]
+my_points = [wkt.loads(i) for i in my_wkts]
+distances = list(range(1, 1000, 499))
 
 
-def test_type():
-    s = gpd.GeoSeries.from_wkt(my_wkts, crs="epsg:4326")
-    buffers = geographic_buffer(s, 1000)
+@pytest.mark.parametrize("crs", [None, "epsg:4326"])
+@pytest.mark.parametrize("epsg", [None, 4326])
+def test_to_crs_work(crs, epsg):
+    string_or_int_to_crs(crs, epsg)
 
-    assert all(buffers.type == "Polygon")
 
-
-def test_missing_crs():
+def test_to_crs_missing():
     with tm.assert_produces_warning(UserWarning) as w:
-        s = gpd.GeoSeries.from_wkt(my_wkts)
-        geographic_buffer(s, 1000)
+        string_or_int_to_crs()
 
     msg = str(w[0].message)
-    assert "EPSG:4326" in msg
     assert "missing" in msg
+    assert "EPSG:4326" in msg
+
+
+class TestDealingOneGeometry:
+    def setup_method(self):
+        self.p = my_points[0]
+        self.crs = CRS.from_user_input("epsg:4326")
+        self.distance = 1000
+
+    @pytest.mark.parametrize("geom", my_points)
+    @pytest.mark.parametrize("distance", distances)
+    def test_work(self, geom, distance):
+        _geographic_buffer(geom, distance, self.crs)
+
+    def test_geometry_is_none(self):
+        assert _geographic_buffer(None, self.distance, self.crs) is None
+
+    def test_distance_type_is_not_num_type(self):
+        with pytest.raises(TypeError):
+            _geographic_buffer(self.p, np.asarray([self.distance]), self.crs)
+
+    def test_distance_less_then_zero(self):
+        with pytest.raises(ValueError):
+            _geographic_buffer(self.p, -1000, crs=self.crs)
+
+
+class TestDealingMultipleGeometry:
+    def setup_method(self):
+        self.s = gpd.GeoSeries.from_wkt(my_wkts, crs="epsg:4326")
+        self.crs = CRS.from_user_input("epsg:4326")
+
+    @pytest.mark.parametrize(
+        "distance", [1000, distances, np.asarray(distances), pd.Series(distances)]
+    )
+    def test_distance_work(self, distance):
+        geographic_buffer(self.s, distance)
+
+    def test_distance_is_pd_series(self):
+        df_distance = pd.Series(range(1, 1000, 499))
+        geographic_buffer(self.s, df_distance)
+
+    def test_distance_index_is_different_to_data(self):
+        df_distance = pd.Series(range(1, 1000, 499), index=["a", "b", "c"])
+        with pytest.raises(IndexError):
+            geographic_buffer(self.s, df_distance)
+
+    def test_distance_length_is_different_to_data(self):
+        with pytest.raises(IndexError):
+            geographic_buffer(self.s, [1, 1000])
