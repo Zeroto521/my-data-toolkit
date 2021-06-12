74fd1f1e68282095979e15fab9ab8de80dc77be7
Zero <Zeroto521@gmail.com>
2021-06-12 21:57:44 +0800

    TYP: add returns type

--- dtoolkit/transformer.py
+++ dtoolkit/transformer.py
@@ -1,14 +1,15 @@
-from typing import Any, List
+from __future__ import annotations
+
+from typing import List
 
 import pandas as pd
 from pandas.util._decorators import doc
 from sklearn.base import TransformerMixin
 
-from ._typing import Pd
+from ._typing import L, Pd
 from .accessor import PandasColumnAccessor  # noqa
 
 
-@doc(TransformerMixin)
 class DummifierTF(TransformerMixin):
     def __init__(self, cols: List[str] = None):
         self.cols = cols
@@ -23,11 +24,10 @@ class DummifierTF(TransformerMixin):
         else:
             return pd.get_dummies(X, columns=X.col.columns, **params)
 
-    def fit_transform(self, X: Pd, *_):
+    def fit_transform(self, X: Pd, *_) -> pd.DataFrame:
         return self.transform(X)
 
 
-@doc(TransformerMixin)
 class SelectorTF(TransformerMixin):
     def __init__(self, cols: List[str] = None):
         self.cols = cols
@@ -35,11 +35,11 @@ class SelectorTF(TransformerMixin):
     def fit(self, *_):
         return self
 
-    def transform(self, X: Pd):
+    def transform(self, X: Pd) -> Pd:
         return X[self.cols] if self.cols else X
 
-    def fit_transform(self, X: Pd, *_):
+    def fit_transform(self, X: Pd, *_) -> Pd:
         return self.transform(X)
 
-    def inverse_transform(self, X: Any, *_):
+    def inverse_transform(self, X: L, *_) -> L:
         return X
