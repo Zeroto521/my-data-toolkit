0ac6983bd98b53f9333f8c902180fd59f6e22ebd
Zero <Zeroto521@gmail.com>
2021-06-14 22:28:51 +0800

    CLN: use onehot transfomer is fine
    see https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.OneHotEncoder.html

--- dtoolkit/transformer.py
+++ dtoolkit/transformer.py
@@ -3,33 +3,14 @@ from __future__ import annotations
 from typing import List
 
 import pandas as pd
-from pandas.util._decorators import doc
 from sklearn.base import TransformerMixin
 
 from ._typing import Pd
-from .accessor import PandasColumnAccessor  # noqa
 
 
 class TransformerBase(TransformerMixin):
     def fit(self):
         return self
-
-
-class DummifierTF(TransformerBase):
-    def __init__(self, cols: List[str] = None):
-        self.cols = cols
-
-    @doc(pd.get_dummies)
-    def transform(self, X: Pd, **params) -> pd.DataFrame:
-        columns = self.cols or X.cols()
-
-        if self.cols:
-            X = X[self.cols]
-
-        return pd.get_dummies(X, columns=columns, **params)
-
-    def fit_transform(self, X: Pd, *_) -> pd.DataFrame:
-        return self.transform(X)
 
 
 class SelectorTF(TransformerBase):
