2f055ee5bb57d3d18a7ecf94e7503b7d9feb136a
Zero <Zeroto521@gmail.com>
2021-06-14 09:12:51 +0800

    EHN: api changed, `xx.col.columns` -> `xx.cols()`
    hook in method not object

--- dtoolkit/tests/test_column.py
+++ dtoolkit/tests/test_column.py
@@ -1,14 +1,14 @@
 import pandas as pd
+import pytest
 from dtoolkit.accessor import PandasColumnAccessor  # noqa
 
-
-def test_series_column():
-    column = "item"
-    s = pd.Series(range(10), name=column)
-    assert s.col.columns == column
+s = pd.Series(range(10), name="item")
+d = pd.DataFrame({"a": range(10), "b": range(10)})
 
 
-def test_dataframe_column():
-    data = {"a": range(10), "b": range(10)}
-    d = pd.DataFrame(data)
-    assert list(d.col.columns) == list(data.keys())
+@pytest.mark.parametrize("df", [s, d])
+def test_column(df):
+    if isinstance(df, pd.Series):
+        assert df.cols() == df.name
+    else:
+        assert (df.cols() == df.columns).all()
