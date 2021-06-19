8b3814f1bee6761a78f559ed3e4caaf210ee47db
Zero <Zeroto521@gmail.com>
2021-06-19 20:04:42 +0800

    EHN: consider `GeometryCollection` condition

--- dtoolkit/coordinate.py
+++ dtoolkit/coordinate.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List
+from typing import Tuple
 from warnings import warn
 
 import geopandas as gpd
@@ -11,25 +11,45 @@ from ._checking import check_geometry_type
 from ._typing import CoordLenType, CoordType
 
 
-def coords(df: gpd.GeoSeries | gpd.GeoDataFrame) -> List[CoordType]:
-    return [_coords(g) for g in df.geometry]
+def coords(
+    df: gpd.GeoSeries | gpd.GeoDataFrame,
+) -> Tuple[CoordType] | Tuple[Tuple[CoordType]]:
+
+    return (_coords(g) for g in df.geometry)
 
 
-def coords_num(df: gpd.GeoSeries | gpd.GeoDataFrame) -> List[CoordLenType]:
-    return [_coords_num(g) for g in df.geometry]
+def coords_num(
+    df: gpd.GeoSeries | gpd.GeoDataFrame,
+) -> Tuple[CoordLenType] | Tuple[Tuple[CoordLenType]]:
+
+    return (_coords_num(g) for g in df.geometry)
 
 
-def _coords(geom: BaseGeometry) -> CoordType:
+def _coords(geom: BaseGeometry) -> CoordType | Tuple[CoordType]:
     check_geometry_type(geom)
 
     geom_dic = mapping(geom)
-    return geom_dic["coordinates"]
+    return _get_coords(geom_dic)
+
+
+def _get_coords(geom_dic: dict) -> CoordType | Tuple[CoordType]:
+    if geom_dic.get("type") != "GeometryCollection":
+        return geom_dic.get("coordinates")
+
+    geoms = geom_dic.get("geometries")
+    return (_get_coords(geom) for geom in geoms)
 
 
 def _coords_num(geom: BaseGeometry) -> CoordLenType:
     check_geometry_type(geom)
 
-    typ, coordinates = mapping(geom)
+    geom_dic = mapping(geom)
+    return _get_coords_num(geom_dic)
+
+
+def _get_coords_num(geom_dic: dict) -> CoordLenType:
+    typ = geom_dic.get("type")
+    coordinates = geom_dic.get("coordinates")
 
     if typ == "Point":
         return 1
@@ -39,7 +59,9 @@ def _coords_num(geom: BaseGeometry) -> CoordLenType:
         return (len(c) for c in coordinates)
     elif typ == "MultiPolygon":
         return ((len(c) for c in g) for g in coordinates)
+    elif typ == "GeometryCollection":
+        geoms = geom_dic.get("geometries")
+        return (_get_coords_num(geom) for geom in geoms)
     else:
         warn("Unknown type %s" % typ, UserWarning)
         return 0
-
