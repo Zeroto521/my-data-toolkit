37d6c18477616083e8ff174149612becf2dcaa60
Zero <Zeroto521@gmail.com>
2021-06-20 10:16:20 +0800

    TST, EHN: add test examples and count coordinate number
    - TST: add test examples for `coordinate.py`
    - EHN: add new method to count all coordinates number
    - EHN: add geopandas type check function
    - EHN: make the `coords` return is tuple

--- dtoolkit/coordinate.py
+++ dtoolkit/coordinate.py
@@ -3,26 +3,37 @@ from __future__ import annotations
 from typing import Tuple
 from warnings import warn
 
-import geopandas as gpd
+import pandas as pd
+from more_itertools import collapse
 from shapely.geometry import mapping
 from shapely.geometry.base import BaseGeometry
 
-from ._checking import check_geometry_type
-from ._typing import CoordLenType, CoordType
+from ._checking import check_geometry_type, check_geopandas_type, istype
+from ._typing import CoordNumType, CoordType, GPd, NumericBaseList, NumericBaseType
 
 
-def coords(
-    df: gpd.GeoSeries | gpd.GeoDataFrame,
-) -> Tuple[CoordType] | Tuple[Tuple[CoordType]]:
+def coords(df: GPd) -> pd.Series:
+    check_geopandas_type(df)
 
-    return (_coords(g) for g in df.geometry)
+    return df.geometry.apply(_coords)
 
 
-def coords_num(
-    df: gpd.GeoSeries | gpd.GeoDataFrame,
-) -> Tuple[CoordLenType] | Tuple[Tuple[CoordLenType]]:
+def coords_numlist(df: GPd) -> pd.Series:
+    check_geopandas_type(df)
 
-    return (_coords_num(g) for g in df.geometry)
+    return df.geometry.apply(_coords_num)
+
+
+def coords_num(df: GPd) -> pd.Series:
+    res = coords_numlist(df)
+
+    def sum_nums(nums: CoordNumType) -> NumericBaseType:
+        if istype(nums, NumericBaseList):
+            return nums
+        else:
+            return sum(collapse(nums, levels=1))
+
+    return res.apply(sum_nums)
 
 
 def _coords(geom: BaseGeometry) -> CoordType | Tuple[CoordType]:
@@ -34,20 +45,20 @@ def _coords(geom: BaseGeometry) -> CoordType | Tuple[CoordType]:
 
 def _get_coords(geom_dic: dict) -> CoordType | Tuple[CoordType]:
     if geom_dic.get("type") != "GeometryCollection":
-        return geom_dic.get("coordinates")
+        return tuple(geom_dic.get("coordinates"))
 
     geoms = geom_dic.get("geometries")
     return (_get_coords(geom) for geom in geoms)
 
 
-def _coords_num(geom: BaseGeometry) -> CoordLenType:
+def _coords_num(geom: BaseGeometry) -> CoordNumType:
     check_geometry_type(geom)
 
     geom_dic = mapping(geom)
     return _get_coords_num(geom_dic)
 
 
-def _get_coords_num(geom_dic: dict) -> CoordLenType:
+def _get_coords_num(geom_dic: dict) -> CoordNumType:
     typ = geom_dic.get("type")
     coordinates = geom_dic.get("coordinates")
 
@@ -56,12 +67,12 @@ def _get_coords_num(geom_dic: dict) -> CoordLenType:
     elif typ in ("LineString", "LinearRing", "MultiPoint"):
         return len(coordinates)
     elif typ in ("Polygon", "MultiLineString"):
-        return (len(c) for c in coordinates)
+        return tuple(len(c) for c in coordinates)
     elif typ == "MultiPolygon":
-        return ((len(c) for c in g) for g in coordinates)
+        return tuple(tuple(len(c) for c in g) for g in coordinates)
     elif typ == "GeometryCollection":
         geoms = geom_dic.get("geometries")
-        return (_get_coords_num(geom) for geom in geoms)
+        return tuple(_get_coords_num(geom) for geom in geoms)
     else:
         warn("Unknown type %s" % typ, UserWarning)
         return 0
