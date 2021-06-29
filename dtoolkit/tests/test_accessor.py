60c66143202e444bb66350ea09203231f6adc57a
Zero <Zeroto521@gmail.com>
2021-06-29 15:41:34 +0800

    TST: add test example for `DropInfAccessor`

--- dtoolkit/tests/test_accessor.py
+++ dtoolkit/tests/test_accessor.py
@@ -1,9 +1,12 @@
+import numpy as np
 import pandas as pd
 import pytest
 from dtoolkit.accessor import ColumnAccessor  # noqa
+from dtoolkit.accessor import DropInfAccessor  # noqa
 
 s = pd.Series(range(10), name="item")
 d = pd.DataFrame({"a": range(10), "b": range(10)})
+s_inf = pd.Series([np.inf, -np.inf])
 
 
 @pytest.mark.parametrize("df", [s, d])
@@ -12,3 +15,8 @@ def test_columnaccessor(df):
         assert df.cols() == df.name
     else:
         assert (df.cols() == df.columns).all()
+
+
+@pytest.mark.parametrize("df", [s, s.append(s_inf)])
+def test_dropinfaccessor(df):
+    assert (df.dropinf() == s).all()
