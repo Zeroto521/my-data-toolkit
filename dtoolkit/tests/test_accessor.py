3636200d5e87d3dbabe88aad97d5ae18ae20d5b4
Zero <Zeroto521@gmail.com>
2021-07-22 10:58:15 +0800

    TST: test filterin accessor

--- dtoolkit/tests/test_accessor.py
+++ dtoolkit/tests/test_accessor.py
@@ -4,12 +4,20 @@ import pytest
 
 from dtoolkit.accessor import ColumnAccessor  # noqa
 from dtoolkit.accessor import DropInfAccessor  # noqa
+from dtoolkit.accessor import FilterInAccessor  # noqa
 
 
-s = pd.Series(range(10), name="item")
+data_size = 10
+s = pd.Series(range(data_size), name="item")
 s = s.astype(float)
 
-d = pd.DataFrame({"a": range(10), "b": range(10)})
+label_size = 3
+d = pd.DataFrame(
+    {
+        "a": np.random.randint(label_size, size=data_size),
+        "b": np.random.randint(label_size, size=data_size),
+    },
+)
 s_inf = pd.Series([np.inf, -np.inf])
 
 
@@ -24,3 +32,23 @@ def test_columnaccessor(df):
 @pytest.mark.parametrize("df", [s, s.append(s_inf)])
 def test_dropinfaccessor(df):
     assert s.equals(df.dropinf())
+
+
+class TestFilterInAccessor:
+    def setup_method(self):
+        self.d = d.copy(True)
+        self.condition = {"a": [0, 1], "b": [2]}
+
+    def test_work(self):
+        res = self.d.filterin(self.condition)
+
+        assert res["a"].isin([0, 1]).any()  # 0 and 1 in a
+        assert (~res["a"].isin([2])).all()  # 2 not in a
+        assert res["b"].isin([2]).any()  # 2 in a
+        assert (~res["b"].isin([0, 1])).all()  # 0 and not in a
+
+    def test_inplace_is_true(self):
+        res = self.d.filterin(self.condition, inplace=True)
+
+        assert res is None
+        assert self.d.equals(d) is False
