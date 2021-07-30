3c579ae85f9b81c9dd382c30b82376a98ed64041
Zero <Zeroto521@gmail.com>
2021-07-30 16:42:53 +0800

    DOC: remove print

--- dtoolkit/transformer/sklearn.py
+++ dtoolkit/transformer/sklearn.py
@@ -217,16 +217,16 @@ class MinMaxScaler(SKMinMaxScaler):
     >>> from dtoolkit.transformer import MinMaxScaler
     >>> data = [[-1, 2], [-0.5, 6], [0, 10], [1, 18]]
     >>> scaler = MinMaxScaler()
-    >>> print(scaler.fit(data))
+    >>> scaler.fit(data)
     MinMaxScaler()
-    >>> print(scaler.data_max_)
+    >>> scaler.data_max_
     [ 1. 18.]
-    >>> print(scaler.transform(data))
+    >>> scaler.transform(data)
     [[0.   0.  ]
      [0.25 0.25]
      [0.5  0.5 ]
      [1.   1.  ]]
-    >>> print(scaler.transform([[2, 2]]))
+    >>> scaler.transform([[2, 2]])
     [[1.5 0. ]]
 
     Notes
