58b15c2000d89e4a588fcefea174acb3c759ffc3
Zero <Zeroto521@gmail.com>
2021-07-30 10:32:20 +0800

    DOC: finished evaltf doc

--- dtoolkit/transformer/pandas.py
+++ dtoolkit/transformer/pandas.py
@@ -267,6 +267,92 @@ class DropTF(DataFrameTF):
 
 
 class EvalTF(DataFrameTF):
+    """
+    Evaluate a string describing operations on DataFrame columns.
+
+    Operates on columns only, not specific rows or elements.  This allows
+    `eval` to run arbitrary code, which can make you vulnerable to code
+    injection if you pass user input to this function.
+
+    Parameters
+    ----------
+    expr : str
+        The expression string to evaluate.
+    inplace : bool, default False
+        If the expression contains an assignment, whether to perform the
+        operation inplace and mutate the existing DataFrame. Otherwise,
+        a new DataFrame is returned.
+    **kwargs
+        See the documentation for :func:`eval` for complete details
+        on the keyword arguments accepted by :meth:`~pandas.DataFrame.query`.
+
+    Returns
+    -------
+    ndarray, scalar, pandas object
+        The result of the evaluation
+
+    Notes
+    -----
+        `DataFrame.drop`'s `inplace` parameter is not work for transformer.
+        Actually this break pipeline stream. If a transformer's `inplace` is
+        `True`, the next tf input would get `None`.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> from dtoolkit.transformer EvalTF
+    >>> df = pd.DataFrame({'A': range(1, 6), 'B': range(10, 0, -2)})
+    >>> df
+        A   B
+    0  1  10
+    1  2   8
+    2  3   6
+    3  4   4
+    4  5   2
+    >>> tf = EvalTF('A + B')
+    >>> tf.transform(df)
+    0    11
+    1    10
+    2     9
+    3     8
+    4     7
+    dtype: int64
+
+    Assignment is allowed though by default the original DataFrame is not
+    modified.
+
+    >>> tf = EvalTF('C = A + B')
+    >>> tf.transform(df)
+        A   B   C
+    0  1  10  11
+    1  2   8  10
+    2  3   6   9
+    3  4   4   8
+    4  5   2   7
+    >>> df
+        A   B
+    0  1  10
+    1  2   8
+    2  3   6
+    3  4   4
+    4  5   2
+
+    Multiple columns can be assigned to using multi-line expressions:
+
+    >>> tf = EvalTF(
+    ...     '''
+    ... C = A + B
+    ... D = A - B
+    ... '''
+    ... )
+    >>> tf.transform(df)
+        A   B   C  D
+    0  1  10  11 -9
+    1  2   8  10 -6
+    2  3   6   9 -3
+    3  4   4   8  0
+    4  5   2   7  3
+    """
     def operate(self, *args, **kwargs) -> Pd:
         return DataFrame.eval(*args, **kwargs)
 
