5352ef2ecb9cfcb02fbebbdacdf279cc36f2e55c
Zero <Zeroto521@gmail.com>
2021-08-03 10:51:55 +0800

    MAINT: lint doc

--- dtoolkit/transformer/numpy.py
+++ dtoolkit/transformer/numpy.py
@@ -17,20 +17,20 @@ class RavelTF(Transformer):
         The elements of `a` (input data) are read using this index order.
 
         * 'C' means to index the elements in row-major, C-style order,
-        with the last axis index changing fastest, back to the first
-        axis index changing slowest.
+            with the last axis index changing fastest, back to the first
+            axis index changing slowest.
 
         * 'F' means to index the elements in column-major, Fortran-style
-        order, with the first index changing fastest, and the last index
-        changing slowest. Note that the 'C' and 'F' options take no account
-        of the memory layout of the underlying array, and only refer to
-        the order of axis indexing.
+            order, with the first index changing fastest, and the last index
+            changing slowest. Note that the 'C' and 'F' options take no account
+            of the memory layout of the underlying array, and only refer to
+            the order of axis indexing.
 
         * 'A' means to read the elements in Fortran-like index order if
-        `a` is Fortran *contiguous* in memory, C-like order otherwise.
+            `a` is Fortran *contiguous* in memory, C-like order otherwise.
 
         * 'K' means to read the elements in the order they occur in memory,
-        except for reversing the data when strides are negative.
+            except for reversing the data when strides are negative.
 
         By default, 'C' index order is used.
 
