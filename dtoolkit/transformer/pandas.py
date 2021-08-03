aafd29bae9251f9e6f8e400e8c1161a00fea657b
Zero <Zeroto521@gmail.com>
2021-08-03 15:53:19 +0800

    DOC: simply query transformer's doc

--- dtoolkit/transformer/pandas.py
+++ dtoolkit/transformer/pandas.py
@@ -443,105 +443,20 @@ class GetTF(DataFrameTF):
     pd_method = "get"
 
 
-# QueryTF doc ported with modifications from pandas
-# https://github.com/pandas-dev/pandas/blob/master/pandas/core/frame.py
-
-
 class QueryTF(DataFrameTF):
     """
-    Query the columns of a DataFrame with a boolean expression.
+    A transformer coud query the columns of a :obj:`pandas.DataFrame` with
+    a boolean expression.
 
-    Parameters
-    ----------
-    expr : str
-        The query string to evaluate.
-
-        You can refer to variables
-        in the environment by prefixing them with an '@' character like
-        ``@a + b``.
-
-        You can refer to column names that are not valid Python variable names
-        by surrounding them in backticks. Thus, column names containing spaces
-        or punctuations (besides underscores) or starting with digits must be
-        surrounded by backticks. (For example, a column named "Area (cm^2)"
-        would be referenced as ```Area (cm^2)```). Column names which are
-        Python keywords (like "list", "for", "import", etc) cannot be used.
-
-        For example, if one of your columns is called ``a a`` and you want
-        to sum it with ``b``, your query should be ```a a` + b``.
-
-    **kwargs
-        See the documentation for :func:`eval` for complete details
-        on the keyword arguments accepted by :meth:`DataFrame.query`.
-
-    Returns
-    -------
-    DataFrame
-        DataFrame resulting from the provided query expression.
+    See Also
+    --------
+    pandas.DataFrame.query : this transformer's prototype method.
 
     Notes
     -----
-    `DataFrame.query`'s `inplace` parameter is not work for transformer.
-    Actually this break pipeline stream. If a transformer's `inplace` is
-    `True`, the next tf input would get `None`.
-
-    The result of the evaluation of this expression is first passed to
-    :attr:`DataFrame.loc` and if that fails because of a
-    multidimensional key (e.g., a DataFrame) then the result will be passed
-    to :meth:`DataFrame.__getitem__`.
-
-    This method uses the top-level :func:`eval` function to
-    evaluate the passed query.
-
-    The :meth:`~pandas.DataFrame.query` method uses a slightly
-    modified Python syntax by default. For example, the ``&`` and ``|``
-    (bitwise) operators have the precedence of their boolean cousins,
-    :keyword:`and` and :keyword:`or`. This *is* syntactically valid Python,
-    however the semantics are different.
-
-    You can change the semantics of the expression by passing the keyword
-    argument ``parser='python'``. This enforces the same semantics as
-    evaluation in Python space. Likewise, you can pass ``engine='python'``
-    to evaluate an expression using Python itself as a backend. This is not
-    recommended as it is inefficient compared to using ``numexpr`` as the
-    engine.
-
-    The :attr:`DataFrame.index` and
-    :attr:`DataFrame.columns` attributes of the
-    :class:`~pandas.DataFrame` instance are placed in the query namespace
-    by default, which allows you to treat both the index and columns of the
-    frame as a column in the frame.
-    The identifier ``index`` is used for the frame index; you can also
-    use the name of the index to identify it in a query. Please note that
-    Python keywords may not be used as identifiers.
-
-    *Backtick quoted variables*
-
-    Backtick quoted variables are parsed as literal Python code and
-    are converted internally to a Python valid identifier.
-    This can lead to the following problems.
-
-    During parsing a number of disallowed characters inside the backtick
-    quoted string are replaced by strings that are allowed as a Python
-    identifier.
-    These characters include all operators in Python, the space character, the
-    question mark, the exclamation mark, the dollar sign, and the euro sign.
-    For other characters that fall outside the ASCII range (U+0001..U+007F)
-    and those that are not further specified in PEP 3131,
-    the query parser will raise an error.
-    This excludes whitespace different than the space character,
-    but also the hashtag (as it is used for comments) and the backtick
-    itself (backtick can also not be escaped).
-
-    In a special case, quotes that make a pair around a backtick can
-    confuse the parser.
-    For example, ```it's` > `that's``` will raise an error,
-    as it forms a quoted string (``'s > `that'``) with a backtick inside.
-
-    See also the Python documentation about lexical analysis
-    (https://docs.python.org/3/reference/lexical_analysis.html)
-    in combination with the source code in
-    :mod:`pandas.core.computation.parsing`.
+    :meth:`DataFrame.query`'s ``inplace`` parameter is not work for this
+    transformer. Actually this break pipeline stream. If a transformer's
+    ``inplace`` is ``True``, the next tf input would get ``None``.
 
     Examples
     --------
