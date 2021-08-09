717f7e9caf38c2d11cddeeefdd8035d2224ecb08
Zero <Zeroto521@gmail.com>
2021-08-09 15:05:58 +0800

    MAINT: lint code

--- dtoolkit/geography/__init__.py
+++ dtoolkit/geography/__init__.py
@@ -1,7 +1,7 @@
 from .buffer import geographic_buffer
 from .coordinate import coords
-from .coordinate import coords_numlist
 from .coordinate import coords_num
+from .coordinate import coords_numlist
 
 __all__ = [
     "geographic_buffer",
