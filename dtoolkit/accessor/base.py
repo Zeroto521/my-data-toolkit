1b331451915edcd12524fb3ba7315e42412ec8f4
Zero <Zeroto521@gmail.com>
2021-08-18 15:31:11 +0800

    EHN: use decorator to simplify accessor

--- dtoolkit/accessor/base.py
+++ dtoolkit/accessor/base.py
@@ -1,34 +1,53 @@
+from __future__ import annotations
+
+from textwrap import dedent
+
+from pandas.util._decorators import doc
+
 from .._typing import Pd
 
 
 class Accessor:
     """
-    :obj:`~pandas.Series` and :obj:`~pandas.DataFrame` objects base class.
+    Hook method or object into :obj:`~pandas.Series` or
+    :obj:`~pandas.DataFrame`.
 
     Parameters
     ----------
-    pd_obj : {:obj:`~pandas.Series`, :obj:`~pandas.DataFrame`}
+    pd_obj : :obj:`~pandas.Series` or :obj:`~pandas.DataFrame`
         pandas's object
 
     See Also
     --------
     pandas.api.extensions.register_series_accessor
     pandas.api.extensions.register_dataframe_accessor
+    """
 
+    def __init__(self, pd_obj: Pd):
+        self.pd_obj = pd_obj
+
+
+@doc(
+    Accessor,
+    dedent(
+        """
     Examples
     --------
-    >>> from dtoolkit.accessor.base import Accessor
+    >>> from dtoolkit.accessor.base import MethodAccessor
     >>> from pandas.api.extensions import register_dataframe_accessor
     >>> from pandas.api.extensions import register_series_accessor
 
     Hook function to :obj:`~pandas.DataFrame` without any other function just
     one (ex: :func:`~pandas.DataFrame.HookingName`)
 
-    >>> @register_series_accessor('HookingName')
-    ... class MyAccessor(Accessor):
-    ...     def __call__(self):
-    ...         ...
+    >>> @register_dataframe_accessor('HookingName')
+    ... class MyAccessor(MethodAccessor):
+    ...     method = yourmethod
     """
+    ),
+)
+class MethodAccessor(Accessor):
+    amethod: callable[..., Pd]
 
-    def __init__(self, pd_obj: Pd):
-        self.pd_obj = pd_obj
+    def __call__(self, *args, **kwargs):
+        return self.amethod(self.pd_obj, *args, **kwargs)
---
+++ dtoolkit/accessor/register.py
@@ -0,0 +1,30 @@
+from pandas.api.extensions import register_dataframe_accessor, register_series_accessor
+
+from .base import MethodAccessor, register_method_factory
+
+
+def register_method_factory(register_accessor: callable) -> callable:
+
+    # based on pandas_flavor/register.py
+    def register_accessor_method(method: callable) -> callable:
+        """
+        Register a function as a method attached to the
+        :obj:`~pandas.DataFrame` or :obj:`~pandas.Series`.
+        """
+
+        @register_accessor(method.__name__)
+        class InnerAccessor(MethodAccessor):
+            amethod = method
+
+        # Must return method itself, otherwise would get None.
+        return method
+
+    return register_accessor_method
+
+
+register_dataframe_method = register_method_factory(
+    register_dataframe_accessor,
+)
+register_series_method = register_method_factory(
+    register_series_accessor,
+)
