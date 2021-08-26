2ce82f9336d753b46517e9710aca4cde6c5fc5fe
Zero <Zeroto521@gmail.com>
2021-08-26 16:32:38 +0800

    DOC: add `methodtf_factory`'s docs

--- dtoolkit/transformer/factory.py
+++ dtoolkit/transformer/factory.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from inspect import isfunction
 
+from ..util import wraps
 from ._util import snake_to_camel
 from .base import MethodTF
 
@@ -11,23 +12,66 @@ def methodtf_factory(
     inverse_transform_method: callable | None = None,
 ) -> MethodTF:
     """
-    Quickly generate a transformer from a method.
+    Quickly generate transformer from methods.
 
     Parameters
     ----------
-    transform_method
-    inverse_transform_method
+    transform_method : callable
+        The back algorithm of the `~MethodTF.transform`.
+    inverse_transform_method : callable or None
+        The back algorithm of the {func}`~MethodTF.inverse_transform`.
 
     Examples
     --------
-    ...
+
+    .. code-block:: python
+
+        from __future__ import annotations
+
+        import numpy as np
+
+        from dtoolkit.transformer.factory import methodtf_factory
+
+
+        # Generate a plus/minus constant transformer:
+
+
+        def plus_constant(X: np.ndarray, constant: int | float) -> np.ndarray:
+            '''Plus constant to each element of ``X``'''
+
+            return X + constant
+
+
+        def minus_constant(X: np.ndarray, constant: int | float) -> np.ndarray:
+            '''Minus constant to each element of ``X``'''
+
+            return X - constant
+
+
+        PlusTF = methodtf_factory(plus_constant, minus_constant)
+
+
+        # Use this transformer:
+
+        a = np.array([1, 2, 3])
+        tf = PlusTF(1).update_invargs(1)
+        tf.transform(a)
+        # [2 3 4]
+        tf.inverse_transform(a)
+        # [0 1 2]
     """
 
     if not isfunction(transform_method):
         raise TypeError("transform_method must be a function.")
+    else:
+        transform_method = wraps(transform_method)(
+            staticmethod(transform_method),
+        )
 
     if isfunction(inverse_transform_method):
-        inverse_transform_method = staticmethod(inverse_transform_method)
+        inverse_transform_method = wraps(inverse_transform_method)(
+            staticmethod(inverse_transform_method),
+        )
     elif inverse_transform_method is not None:
         raise TypeError("inverse_transform_method must be a function.")
 
@@ -37,7 +81,7 @@ def methodtf_factory(
         classname,
         (MethodTF,),
         dict(
-            transform_method=staticmethod(transform_method),
+            transform_method=transform_method,
             inverse_transform_method=inverse_transform_method,
         ),
     )
