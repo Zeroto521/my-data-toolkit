dac065333c8868d8af229894f18315d5621cebe7
Zero <Zeroto521@gmail.com>
2021-08-30 17:56:50 +0800

    MAINT: drop `check_number_tyep` and `check_greater_than_zero`

--- dtoolkit/geography/_validation.py
+++ dtoolkit/geography/_validation.py
@@ -1,7 +1,5 @@
 from shapely.geometry.base import BaseGeometry
 
-from .._typing import NumericType
-from .._typing import NumericTypeList
 from ..util._validation import istype
 from ._typing import GeoPandasList
 from ._typing import GPd
@@ -15,21 +13,7 @@ def check_geopandas_type(df: GPd):
 
 
 def check_geometry_type(geom: BaseGeometry):
-    if not istype(geom, BaseGeometry):
+    if not isinstance(geom, BaseGeometry):
         raise TypeError(
             f"{geom} must be Geometry.",
-        )
-
-
-def check_number_tyep(num: NumericType):
-    if not istype(num, NumericTypeList):
-        raise TypeError(
-            f"The type of {num} must be int or float.",
-        )
-
-
-def check_greater_than_zero(num: NumericType):
-    if num <= 0:
-        raise ValueError(
-            f"The {num} must be greater than 0.",
         )
