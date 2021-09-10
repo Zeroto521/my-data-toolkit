67584d50b5925e2922df3717b24c04690b34626f
Zero <Zeroto521@gmail.com>
2021-09-10 12:35:00 +0800

    MAINT: rename the parameter of `frame_in_frame_out`

--- dtoolkit/transformer/_decorator.py
+++ dtoolkit/transformer/_decorator.py
@@ -3,15 +3,15 @@ from functools import wraps
 from ._util import transform_array_to_frame
 
 
-def frame_in_frame_out(func):
+def frame_in_frame_out(wrapped):
     """
     Wrap :func:`~dtoolkit.transformer._util.transform_array_to_frame` to
     decorator.
     """
 
-    @wraps(func)
+    @wraps(wrapped)
     def decorator(self, X):
-        X_new = func(self, X)
+        X_new = wrapped(self, X)
 
         return transform_array_to_frame(X_new, X)
 
