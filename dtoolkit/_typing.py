e65796c5fe87bc7c23d05f0f0ccf615b6f74fa7a
Zero <Zeroto521@gmail.com>
2021-09-22 10:52:30 +0800

    MAINT: py3.7 `typing.get_args` compat

--- dtoolkit/_typing.py
+++ dtoolkit/_typing.py
@@ -3,3 +3,16 @@ from typing import Union
 import pandas as pd
 
 SeriesOrFrame = Union[pd.Series, pd.DataFrame]
+
+#
+# typing compat
+#
+
+try:
+    # this function new in 3.8
+    from typing import get_args
+
+except ImportError:
+
+    def get_args(tp) -> tuple:
+        return tp.__args__
