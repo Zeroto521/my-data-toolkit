8b7884e0894def5dc46a97216c0ec3b4a6781356
Zero <Zeroto521@gmail.com>
2021-09-22 11:28:54 +0800

    MAINT: remove transformer._typing

--- dtoolkit/transformer/_typing.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from typing import Callable
-from typing import Union
-
-import numpy as np
-import pandas as pd
-
-Callable[[Union[np.ndarray, pd.DataFrame]], np.ndarray]
