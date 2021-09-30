874da8db438aa352dc3c4229af1598702f04f04d
Zero <Zeroto521@gmail.com>
2021-09-30 17:35:28 +0800

    TST: test series's `top_n`

--- dtoolkit/test/accessor/test_series.py
+++ dtoolkit/test/accessor/test_series.py
@@ -6,6 +6,7 @@ from . import s
 from . import s_inf
 from dtoolkit.accessor.series import bin  # noqa
 from dtoolkit.accessor.series import dropinf  # noqa
+from dtoolkit.accessor.series import top_n  # noqa
 
 
 class TestDropinf:
@@ -106,3 +107,72 @@ class TestBin:
 
         assert res is None
         assert self.s.equals(excepted)
+
+
+class TestTopN:
+    def setup_method(self):
+        self.s = pd.Series(
+            {
+                "Italy": 59000000,
+                "France": 65000000,
+                "Brunei": 434000,
+                "Malta": 434000,
+                "Maldives": 434000,
+                "Iceland": 337000,
+                "Nauru": 11300,
+                "Tuvalu": 11300,
+                "Anguilla": 11300,
+                "Montserrat": 5200,
+            },
+        )
+
+    @pytest.mark.parametrize(
+        "n, largest, keep, excepted",
+        [
+            (
+                4,
+                True,
+                "first",
+                {
+                    "France": 65000000,
+                    "Italy": 59000000,
+                    "Brunei": 434000,
+                    "Malta": 434000,
+                },
+            ),
+            (
+                4,
+                True,
+                "all",
+                {
+                    "France": 65000000,
+                    "Italy": 59000000,
+                    "Brunei": 434000,
+                    "Malta": 434000,
+                    "Maldives": 434000,
+                },
+            ),
+            (
+                2,
+                False,
+                "last",
+                {"Montserrat": 5200, "Anguilla": 11300},
+            ),
+            (
+                2,
+                False,
+                "all",
+                {
+                    "Montserrat": 5200,
+                    "Nauru": 11300,
+                    "Tuvalu": 11300,
+                    "Anguilla": 11300,
+                },
+            ),
+        ],
+    )
+    def test_work(self, n, largest, keep, excepted):
+        result = self.s.top_n(n, largest, keep=keep)
+        excepted = pd.Series(excepted)
+
+        assert result.equals(excepted)
