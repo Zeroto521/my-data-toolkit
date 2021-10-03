c46c29122d8691f390186036d9f6631211a2352e
Zero <Zeroto521@gmail.com>
2021-10-03 09:34:30 +0800

    TST: add basic test for `top_n`

--- dtoolkit/test/accessor/test_dataframe.py
+++ dtoolkit/test/accessor/test_dataframe.py
@@ -6,6 +6,7 @@ from . import d
 from dtoolkit.accessor.dataframe import dropinf  # noqa
 from dtoolkit.accessor.dataframe import filterin  # noqa
 from dtoolkit.accessor.dataframe import repeat  # noqa
+from dtoolkit.accessor.dataframe import top_n  # noqa
 
 
 class TestDropinf:
@@ -221,3 +222,122 @@ class TestRepeat:
     def test_error(self, axis):
         with pytest.raises(ValueError):
             self.d.repeat(2, axis)
+
+
+class TestTopN:
+    @pytest.mark.parametrize(
+        "n, largest, keep, prefix, delimiter, excepted",
+        [
+            (
+                1,
+                True,
+                "first",
+                "top",
+                "_",
+                {
+                    "top_1": [
+                        ("b", 3),
+                        ("a", 3),
+                        ("c", 3),
+                    ],
+                },
+            ),
+            (  # test 'n'
+                2,
+                True,
+                "first",
+                "top",
+                "_",
+                {
+                    "top_1": [("b", 3), ("a", 3), ("c", 3)],
+                    "top_2": [("c", 2), ("b", 2), ("a", 2)],
+                },
+            ),
+            (  # test 'largest'
+                1,
+                False,
+                "first",
+                "top",
+                "_",
+                {
+                    "top_1": [("a", 1), ("c", 1), ("b", 1)],
+                },
+            ),
+            (  # test 'prefix' and 'delimiter'
+                1,
+                True,
+                "first",
+                "largest",
+                "-",
+                {
+                    "largest-1": [
+                        ("b", 3),
+                        ("a", 3),
+                        ("c", 3),
+                    ],
+                },
+            ),
+        ],
+    )
+    def test_single_index_work(
+        self,
+        n,
+        largest,
+        keep,
+        prefix,
+        delimiter,
+        excepted,
+    ):
+        df = pd.DataFrame(
+            {
+                "a": [1, 3, 2],
+                "b": [3, 2, 1],
+                "c": [2, 1, 3],
+            },
+        )
+
+        result = df.top_n(
+            n=n,
+            largest=largest,
+            keep=keep,
+            prefix=prefix,
+            delimiter=delimiter,
+        )
+
+        excepted = pd.DataFrame(excepted)
+        assert result.equals(excepted)
+
+    @pytest.mark.parametrize(
+        "n, keep, excepted",
+        [
+            (
+                1,
+                "first",
+                {
+                    "top_1": [("c", 3), ("a", 3)],
+                },
+            ),
+            (
+                1,
+                "all",
+                {
+                    "top_1": [("c", 3), ("a", 3)],
+                    "top_2": [np.nan, ("b", 3)],
+                    "top_3": [np.nan, ("c", 3)],
+                },
+            ),
+        ],
+    )
+    def test_duplicate_dataframe(self, n, keep, excepted):
+        df = pd.DataFrame(
+            {
+                "a": [1, 3],
+                "b": [2, 3],
+                "c": [3, 3],
+            },
+        )
+
+        result = df.top_n(n=n, keep=keep)
+        excepted = pd.DataFrame(excepted)
+
+        assert result.equals(excepted)
