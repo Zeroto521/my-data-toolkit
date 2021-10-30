3b3bdfb09b0c1e1cad2df16edcedccebd72c3606
Zero <Zeroto521@gmail.com>
2021-10-30 09:25:10 +0800

    MAINT: for supporting geodataframe

--- dtoolkit/geoaccessor/geoseries.py
+++ dtoolkit/geoaccessor/geoseries.py
@@ -1,28 +1,28 @@
 from __future__ import annotations
 
 import geopandas as gpd
+import numpy as np
 import pandas as pd
 from pandas.util._decorators import doc
 from pyproj import CRS
 
 from dtoolkit._typing import OneDimArray
-from dtoolkit.geoaccessor._util import is_int_or_float
 from dtoolkit.geoaccessor._util import string_or_int_to_crs
 from dtoolkit.geoaccessor.register import register_geoseries_method
 from dtoolkit.geoaccessor.tool import geographic_buffer
 
 
 @register_geoseries_method
-@doc(klass="GeoSeries")
+@doc(klass="GeoSeries", alias="s")
 def geobuffer(
-    df: gpd.GeoSeries,
+    s: gpd.GeoSeries,
     distance: int | float | OneDimArray,
     crs: str | None = None,
     epsg: int | None = None,
     **kwargs,
 ) -> gpd.GeoSeries:
     """
-    Creates a geographic buffer.
+    Creates geographic buffers for {klass}.
 
     Creates a buffer zone of specified size around or inside geometry. It
     is designed for use with features in Geographic coordinates. Reprojects
@@ -31,13 +31,13 @@ def geobuffer(
 
     Parameters
     ----------
-    df : {klass}
+    {alias} : {klass}
         Only support `Point` geometry, at present.
 
     distance : int, float, ndarray or Series, the unit is meter.
         The radius of the buffer. If :obj:`~numpy.ndarray` or
         :obj:`~pandas.Series` are used then it must have same length as the
-        :obj:`~geopandas.GeoSeries`.
+        ``{alias}``.
 
     crs : str, optional
         If ``epsg`` is specified, the value can be anything accepted by
@@ -46,12 +46,12 @@ def geobuffer(
 
     epsg : int, optional
 
-        * If ``df.crs`` is not None, the result would use the CRS of
+        * If ``{alias}.crs`` is not None, the result would use the CRS of
           :obj:`~geopandas.GeoSeries`.
-        * If ``df.crs`` is None, the result would use the CRS from ``crs`` or
-          ``epsg``.
+        * If ``{alias}.crs`` is None, the result would use the CRS from ``crs``
+          or ``epsg``.
         * If ``crs`` is specified EPSG code specifying output projection.
-        * If ``df.crs`` is ``None``, the result would use `EPSG:4326`
+        * If ``{alias}.crs`` is ``None``, the result would use `EPSG:4326`
 
     Returns
     -------
@@ -59,35 +59,32 @@ def geobuffer(
 
     See Also
     --------
-    dtoolkit.geoaccessor.tool.geographic_buffer : The core algorithm for creating
-        geographic buffer.
+    dtoolkit.geoaccessor.geoseries.geobuffer
+        Creates geographic buffers for GeoSeries.
+    dtoolkit.geoaccessor.geodataframe.geobuffer
+        Creates geographic buffers for GeoDataFrame1.
+    dtoolkit.geoaccessor.tool.geographic_buffer
+        The core algorithm for creating geographic buffer.
     shapely.geometry.base.BaseGeometry.buffer
         https://shapely.readthedocs.io/en/latest/manual.html#object.buffer
     """
 
-    if isinstance(distance, pd.Series) and not df.index.equals(distance.index):
+    if isinstance(distance, np.ndarray) and len(distance) != len(s):
         raise IndexError(
-            "Index values of distance sequence does "
-            "not match index values of the GeoSeries",
+            f"Length of 'distance' doesn't match length of the {type(s)!r}.",
+        )
+    if isinstance(distance, pd.Series) and not s.index.equals(distance.index):
+        raise IndexError(
+            "Index values of 'distance' sequence doesn't "
+            f"match index values of the {type(s)!r}",
         )
 
-    crs: CRS = df.crs or string_or_int_to_crs(crs, epsg)
-
-    if is_int_or_float(distance):
-        result = df.apply(
-            geographic_buffer,
-            distance=distance,
-            crs=crs,
-            **kwargs,
-        )
-    elif len(distance) != len(df):
-        raise IndexError(
-            f"Length of 'distance' doesn't match length of the {type(df)!r}.",
+    if isinstance(distance, (np.ndarray, pd.Series)):
+        result = (
+            geographic_buffer(g, d, crs=crs, **kwargs) for g, d in zip(s, distance)
         )
     else:
-        result = (
-            geographic_buffer(geom, distance=dist, crs=crs, **kwargs)
-            for geom, dist in zip(df.geometry, distance)
-        )
+        result = (geographic_buffer(g, distance, crs=crs, **kwargs) for g in s)
 
-    return gpd.GeoSeries(result, crs=crs)
+    crs: CRS = s.crs or string_or_int_to_crs(crs, epsg)
+    return gpd.GeoSeries(result, crs=crs, index=s.index, name=s.name)
