7329e04e1cc3b328ee347c3f535173a3fa7e4697
Zero <Zeroto521@gmail.com>
2021-11-22 11:03:24 +0800

    MAINT: drop `__all__` via `# noqa`

--- dtoolkit/geoaccessor/__init__.py
+++ dtoolkit/geoaccessor/__init__.py
@@ -1,16 +1,6 @@
-from dtoolkit.geoaccessor import geodataframe
-from dtoolkit.geoaccessor import geoseries
-from dtoolkit.geoaccessor.accessor import register_geodataframe_accessor
-from dtoolkit.geoaccessor.accessor import register_geoseries_accessor
-from dtoolkit.geoaccessor.register import register_geodataframe_method
-from dtoolkit.geoaccessor.register import register_geoseries_method
-
-
-__all__ = [
-    "geodataframe",
-    "geoseries",
-    "register_geodataframe_accessor",
-    "register_geoseries_accessor",
-    "register_geodataframe_method",
-    "register_geoseries_method",
-]
+from dtoolkit.geoaccessor import geodataframe  # noqa
+from dtoolkit.geoaccessor import geoseries  # noqa
+from dtoolkit.geoaccessor.accessor import register_geodataframe_accessor  # noqa
+from dtoolkit.geoaccessor.accessor import register_geoseries_accessor  # noqa
+from dtoolkit.geoaccessor.register import register_geodataframe_method  # noqa
+from dtoolkit.geoaccessor.register import register_geoseries_method  # noqa
