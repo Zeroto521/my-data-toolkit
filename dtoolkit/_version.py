bcd382243ea2816acb7a4a0fc7e3907fdce5a236
Zero <Zeroto521@gmail.com>
2021-11-24 16:23:54 +0800

    MAINT: sync version style

--- dtoolkit/_version.py
+++ dtoolkit/_version.py
@@ -40,7 +40,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep440-old"
     cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "dtoolkit-"
     cfg.versionfile_source = "dtoolkit/_version.py"
