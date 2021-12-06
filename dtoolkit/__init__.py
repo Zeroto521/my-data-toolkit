d38d6f309efd1dbf6339c99f45af23966b1c022a
Zero <Zeroto521@gmail.com>
2021-12-06 14:34:12 +0800

    remove duplicated codes

--- dtoolkit/__init__.py
+++ dtoolkit/__init__.py
@@ -2,7 +2,3 @@ from dtoolkit._version import get_versions
 
 __version__ = get_versions()["version"]
 del get_versions
-
-from dtoolkit import _version
-
-__version__ = _version.get_versions()["version"]
