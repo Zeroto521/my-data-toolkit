bdf35fca43d1db3b992e9db9ef9fa8fe673ab082
Zero <Zeroto521@gmail.com>
2022-01-05 10:06:33 +0800

    MAINT: lint codes

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -96,7 +96,7 @@ moved_pages = [
 
 html_additional_pages = {page[0]: "redirect.html" for page in moved_pages}
 
-html_context = {"redirects": {old: new for old, new in moved_pages}}
+html_context = {"redirects": dict(moved_pages)}
 
 
 #  --Options for sphinx extensions -----------------------------------------------
