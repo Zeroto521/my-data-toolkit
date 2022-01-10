5c71d43f4ff2855ff5a8c5ffbec4e9301d2678b0
Zero <Zeroto521@gmail.com>
2022-01-10 10:05:57 +0800

    MAINT: lint codes

--- ci/script/versioneer_update_check.py
+++ ci/script/versioneer_update_check.py
@@ -1,15 +1,16 @@
 import os
 import sys
 
+try:
+    sys.path = sys.path[1:]  # import from the system instead of local
+    from versioneer import newver
+except:
+    sys.exit()
 
-if os.path.exists("versioneer.py"):
-    try:
-        sys.path = sys.path[1:]  # import from the system instead of local
-        from versioneer import newver
-    except:
+if not os.path.exists("versioneer.py"):
+    sys.exit()
+
+for line in open("versioneer.py").readlines()[:5]:
+    if "Version" in line and newver in line:  # don't need to update versioneer
+        print(True)
         sys.exit()
-
-    for line in open("versioneer.py").readlines()[:5]:
-        if "Version" in line and newver in line:  # don't need to update versioneer
-            print(True)
-            sys.exit()
