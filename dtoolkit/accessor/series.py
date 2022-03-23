afda49ee0e2610326de5a6dc5b504912cd5d819f
Zero <Zeroto521@gmail.com>
2022-03-23 22:22:24 +0800

    ENH: new method `query`

--- dtoolkit/accessor/series.py
+++ dtoolkit/accessor/series.py
@@ -660,3 +660,34 @@ def eval(s: pd.Series, expr: str, inplace: bool = False, **kwargs):
             "resolvers": tuple(kwargs.get("resolvers", ())) + resolvers,
         },
     )
+
+
+@register_series_method
+def query(s: pd.Series, expr: str, inplace: bool = False, **kwargs):
+    inplace = validate_bool_kwarg(inplace, "inplace")
+
+    if not isinstance(expr, str):
+        raise ValueError(
+            f"expr must be a string to be evaluated, {type(expr)} given",
+        )
+
+    mask = s.eval(
+        expr,
+        **{
+            **kwargs,
+            "level": kwargs.pop("level", 0) + 1,
+            "target": None,
+        },
+    )
+
+    try:
+        result = s.loc[mask]
+    except ValueError:
+        # when mask is multi-dimensional loc raises, but this is sometimes a
+        # valid query
+        result = s[mask]
+
+    if not inplace:
+        return result
+
+    s._update_inplace(result)
