a7ce1d7871c277d2d0c4e733c9686831ee4f0431
Zero <Zeroto521@gmail.com>
2022-04-03 11:04:23 +0800

    move to _decorator package

---
+++ dtoolkit/_decorator/warning.py
@@ -0,0 +1,41 @@
+from functools import wraps
+from warnings import warn
+
+
+def warning(message: str, category: Exception = None):
+    """
+    A warning decorator.
+
+    Parameters
+    ----------
+    message : str
+        The warning information to user.
+
+    category : Exception, optional
+        If given, must be a **warning category class**. it defaults to
+        :keyword:`UserWarning`.
+
+    See Also
+    --------
+    warnings.warn
+
+    Examples
+    --------
+    >>> from dtoolkit._decorator import warning
+    >>> @warning("This's a warning message.") 
+    ... def func() -> str:
+    ...     return "[Done]."
+    >>> func()
+    '[Done].'
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            warn(message, category=category)
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
