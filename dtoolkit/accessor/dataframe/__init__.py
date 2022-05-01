c43cd398b058cace86e04e7bfa4dc6147f7e59fa
Zero <Zeroto521@gmail.com>
2022-05-01 17:21:15 +0800

    DEP: Drop `unique_counts` method (#512)

--- dtoolkit/accessor/dataframe/__init__.py
+++ dtoolkit/accessor/dataframe/__init__.py
@@ -6,5 +6,4 @@ from dtoolkit.accessor.dataframe.filter_in import filter_in  # noqa
 from dtoolkit.accessor.dataframe.repeat import repeat  # noqa
 from dtoolkit.accessor.dataframe.to_series import to_series  # noqa
 from dtoolkit.accessor.dataframe.top_n import top_n  # noqa
-from dtoolkit.accessor.dataframe.unique_counts import unique_counts  # noqa
 from dtoolkit.accessor.dataframe.values_to_dict import values_to_dict  # noqa
