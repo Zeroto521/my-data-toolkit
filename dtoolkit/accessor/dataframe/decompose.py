6018b6adfb6c8eb31f58f85b909701f8b841f80f
Zero <Zeroto521@gmail.com>
2022-05-02 19:02:50 +0800

    Skip doctest
    Iteration algorithms such as PCA will have different results due to different environments.

--- dtoolkit/accessor/dataframe/decompose.py
+++ dtoolkit/accessor/dataframe/decompose.py
@@ -90,7 +90,7 @@ def decompose(
 
     Decompose all columns.
 
-    >>> df.decompose(decomposition.PCA)
+    >>> df.decompose(decomposition.PCA)  # doctest: +SKIP
               a         b             c             d
     0  1.956431  0.415183  9.009015e-17  8.100537e-18
     1  3.142238 -0.355441  8.394617e-17  9.817066e-18
@@ -101,7 +101,7 @@ def decompose(
 
     Decompose the selected columns.
 
-    >>> df.decompose(decomposition.PCA, ["a", "b"])
+    >>> df.decompose(decomposition.PCA, ["a", "b"])  # doctest: +SKIP
               a         b  c  d
     0  1.383406  0.293579  1  1
     1  2.221898 -0.251335  2  1
@@ -112,7 +112,10 @@ def decompose(
 
     Decompose and remap columns to a few.
 
-    >>> df.decompose(decomposition.PCA, {"A": ["a", "b"], "B": ["b", "c", "d"]})
+    >>> df.decompose(
+    ...     decomposition.PCA,
+    ...     {"A": ["a", "b"], "B": ["b", "c", "d"]}
+    ... )  # doctest: +SKIP
               A         B  a  b  c  d
     0  1.383406  1.694316 -1 -1  1  1
     1  2.221898  2.428593 -2 -1  2  1
@@ -120,7 +123,10 @@ def decompose(
     3 -1.383406 -1.694316  1  1 -1 -1
     4 -2.221898 -2.428593  2  1 -2 -1
     5 -3.605304 -4.122909  3  2 -3 -2
-    >>> df.decompose(decomposition.PCA, {("A", "B"): ["a", "b", "c"]})
+    >>> df.decompose(
+    ...     decomposition.PCA,
+    ...     {("A", "B"): ["a", "b", "c"]}
+    ... )  # doctest: +SKIP
               A         B  a  b  c  d
     0  1.702037  0.321045 -1 -1  1  1
     1  2.988071 -0.267273 -2 -1  2  1
@@ -136,7 +142,7 @@ def decompose(
     A    a
     A    b
     dtype: object
-    >>> df.decompose(decomposition.PCA, s, drop=True)
+    >>> df.decompose(decomposition.PCA, s, drop=True)  # doctest: +SKIP
               A  c  d
     0  1.383406  1  1
     1  2.221898  2  1
