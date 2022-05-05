84e163dfc81abaf0ef12b0a547b25b41b8c2158b
Zero <Zeroto521@gmail.com>
2022-05-05 21:35:59 +0800

    MAINT: Use `build` to build package

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
+import os
+import sys
+
 from setuptools import setup
 
-from versioneer import get_cmdclass
+# Ensure the current directory is on sys.path so versioneer can be imported
+# when pip uses PEP 517/518 build rules.
+# https://github.com/python-versioneer/python-versioneer/issues/193
+sys.path.append(os.path.dirname(__file__))
 
-setup(
-    cmdclass=get_cmdclass(),
-)
+from versioneer import get_cmdclass  # noqa: E402
+
+setup(cmdclass=get_cmdclass())
