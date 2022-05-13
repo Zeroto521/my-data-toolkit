f972ea2a9d20268a72c618d546bfa4311fa5b264
Zero <Zeroto521@gmail.com>
2022-05-13 11:42:57 +0800

    TST: Completed `values_to_dict` suitcases

---
+++ test/accessor/series/test_values_to_dict.py
@@ -0,0 +1,50 @@
+import pytest
+
+import pandas as pd
+
+from dtoolkit.accessor.series import values_to_dict  # noqa
+
+@pytest.mark.parametrize(
+    "s, kwargs, excepted",
+    [
+        (
+            pd.Series(range(4), index=["a", "b", "a", "c"]),
+            dict(to_list=True),
+            {"a": [0, 2], "b": [1], "c": [3]},
+        ),
+        (
+            pd.Series(range(4), index=["a", "b", "a", "c"]),
+            dict(to_list=False),
+            {"a": [0, 2], "b": 1, "c": 3},
+        ),
+        (
+            pd.Series(range(4), index=["a", "b", "c", "d"]),
+            dict(to_list=False),
+            {"a": 0, "b": 1, "c": 2, "d": 3},
+        ),
+        (
+            pd.Series([1]*4, index=["a", "b", "c", "d"]),
+            dict(to_list=True),
+            {"a": [1], "b": [1], "c": [1], "d": [1]},
+        ),
+        (
+            pd.Series([1]*4, index=["a", "b", "c", "d"]),
+            dict(to_list=False),
+            {"a": 1, "b": 1, "c": 1, "d": 1},
+        ),
+        (
+            pd.Series(range(4), index=["a"]*4),
+            dict(to_list=True),
+            {'a': [0, 1, 2, 3]},
+        ),
+        (
+            pd.Series([1]*4, index=["a"]*4),
+            dict(to_list=False),
+            {'a': [1, 1, 1, 1]},
+        ),
+    ],
+)
+def test_work(s, kwargs, excepted):
+    result = s.values_to_dict(**kwargs)
+
+    assert result == excepted
