abaab92c4ae8bba5c99de5f9f54cf971bd417ba4
Zero <Zeroto521@gmail.com>
2022-05-13 20:25:44 +0800

    EHN: New accessor `boolean` for `DataFrame` (#537)
    * EHN: New accessor `bollean` for `DataFrame`
    
    * Remove `get_mask` method
    
    * Index this method at the doc site
    
    * lint codes
    
    * Update suitcase

--- dtoolkit/accessor/_util.py
+++ dtoolkit/accessor/_util.py
@@ -7,9 +7,6 @@ import numpy as np
 if TYPE_CHECKING:
     from typing import Iterable
 
-    from dtoolkit._typing import OneDimArray
-    from dtoolkit._typing import TwoDimArray
-
 
 def get_inf_range(inf: str = "all") -> list[float]:
     if inf == "all":
@@ -22,17 +19,6 @@ def get_inf_range(inf: str = "all") -> list[float]:
         raise ValueError(f"invalid inf option: {inf!r}")
 
     raise TypeError("must specify inf")
-
-
-def get_mask(how: str, mask: TwoDimArray, axis: int) -> OneDimArray:
-    if how == "any":
-        return mask.any(axis=axis)
-    elif how == "all":
-        return mask.all(axis=axis)
-    elif how is not None:
-        raise ValueError(f"invalid inf option: {how!r}")
-
-    raise TypeError("must specify how")
 
 
 # based on more_itertools/more.py
