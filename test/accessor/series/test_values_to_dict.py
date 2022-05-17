06c079953a06618ba2b72270315c73177110514e
Zero <Zeroto521@gmail.com>
2022-05-17 11:36:36 +0800

    TST: Completed series's values_to_dict test cases

---
+++ test/accessor/series/test_values_to_dict.py
@@ -0,0 +1,41 @@
+import pytest
+
+import pandas as pd
+
+from dtoolkit.accessor.series import values_to_dict
+
+
+@pytest.mark.parametrize(
+    "s, kwargs, expected",
+    [
+        (
+            pd.Series(range(4), index=["a", "b", "a", "c"]),
+            dict(unique=True, to_list=True),
+            {"a": [0, 2], "b": [1], "c": [3]},
+        ),
+        (
+            pd.Series(range(4), index=["a", "b", "a", "c"]),
+            dict(unique=False, to_list=False),
+            {"a": [0, 2], "b": 1, "c": 3},
+        ),
+        (
+            pd.Series([0] * 4, index=["a", "b", "a", "c"]),
+            dict(unique=True, to_list=False),
+            {"a": 0, "b": 0, "c": 0},
+        ),
+        (
+            pd.Series([0] * 4, index=["a", "b", "a", "c"]),
+            dict(unique=False, to_list=False),
+            {"a": [0, 0], "b": 0, "c": 0},
+        ),
+        (
+            pd.Series([0] * 4, index=["a", "b", "a", "c"]),
+            dict(unique=True, to_list=True),
+            {"a": [0], "b": [0], "c": [0]},
+        ),
+    ],
+)
+def test_work(s, kwargs, expected):
+    result = s.values_to_dict(**kwargs)
+
+    assert result == expected
