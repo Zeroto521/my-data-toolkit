bee673a061a1548583b725981c6655bef0b30d37
Zero <Zeroto521@gmail.com>
2022-05-21 11:43:20 +0800

    column also support int type

--- dtoolkit/geoaccessor/dataframe/from_wkt.py
+++ dtoolkit/geoaccessor/dataframe/from_wkt.py
@@ -18,7 +18,7 @@ if TYPE_CHECKING:
 @register_dataframe_method()
 def from_wkt(
     df: pd.DataFrame,
-    column: str,
+    column: IntOrStr,
     crs: CRS | IntOrStr = None,
     drop: bool = False,
 ) -> gpd.GeoSeries | gpd.GeoDataFrame:
@@ -30,7 +30,7 @@ def from_wkt(
 
     Parameters
     ----------
-    column : str
+    column : str or int
         The name of WKT column.
 
     crs : CRS, str, int, optional
