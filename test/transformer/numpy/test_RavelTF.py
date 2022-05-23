cc295d90bcc4919ce9c4dc8be4e471e2d836d16c
Zero <Zeroto521@gmail.com>
2022-05-23 16:11:25 +0800

    Remove self

--- test/transformer/numpy/test_RavelTF.py
+++ test/transformer/numpy/test_RavelTF.py
@@ -9,14 +9,14 @@ from dtoolkit.transformer import RavelTF
 
 
 @pytest.mark.parametrize("data", [array, df_iris, s, s.tolist()])
-def test_transform(self, data):
+def test_transform(data):
     result = RavelTF().fit_transform(data)
 
     assert result.ndim == 1
 
 
 @pytest.mark.parametrize("data", [array, df_iris, s, s.tolist()])
-def test_inverse_transform(self, data):
+def test_inverse_transform(data):
     tf = RavelTF()
 
     transformed_data = tf.fit_transform(data)
