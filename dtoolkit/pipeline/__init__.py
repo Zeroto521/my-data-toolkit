ec8797475778615053f789a253e5afcb4910faef
Zero <Zeroto521@gmail.com>
2022-05-24 11:10:35 +0800

    MAINT: Move dtoolkit.transformer.pipeline into dtoolkit.pipeline

---
+++ dtoolkit/pipeline/__init__.py
@@ -0,0 +1,4 @@
+from dtoolkit.pipeline.FeatureUnion import FeatureUnion  # noqa
+from dtoolkit.pipeline.make_pipeline import make_pipeline  # noqa
+from dtoolkit.pipeline.make_union import make_union  # noqa
+from dtoolkit.pipeline.Pipeline import Pipeline  # noqa
