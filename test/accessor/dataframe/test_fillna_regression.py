c81e5c66331ad6789291a35642fded5543fc05f2
Zero <Zeroto521@gmail.com>
2022-05-26 18:35:25 +0800

    TST: test multi-key dictionary

--- test/accessor/dataframe/test_fillna_regression.py
+++ test/accessor/dataframe/test_fillna_regression.py
@@ -217,6 +217,58 @@ from dtoolkit.accessor.dataframe import fillna_regression  # noqa
                 columns=["x1", "x2", "y"],
             ),
         ),
+        (
+            pd.DataFrame(
+                [
+                    [1, 1, None],
+                    [1, 2, 8],
+                    [2, 2, 9],
+                    [2, 3, 11],
+                    [3, 5, None],
+                ],
+                columns=["x1", "x2", "y"],
+            ),
+            tree.DecisionTreeRegressor,
+            {"y": "x2", "y": ["x1"], "y": ["x1", "x2"]},
+            "na",
+            {},
+            pd.DataFrame(
+                [
+                    [1, 1, 6],
+                    [1, 2, 8],
+                    [2, 2, 9],
+                    [2, 3, 11],
+                    [3, 5, 11],
+                ],
+                columns=["x1", "x2", "y"],
+            ),
+        ),
+        (
+            pd.DataFrame(
+                [
+                    [1, 1, None, 6],
+                    [1, 2, 8, 8],
+                    [2, 2, 9, 9],
+                    [2, 3, 11, 11],
+                    [3, 5, None, None],
+                ],
+                columns=["x1", "x2", "y1", "y2"],
+            ),
+            tree.DecisionTreeRegressor,
+            {"y2": ["x1"], "y1": ["x1", "x2"]},
+            "na",
+            {},
+            pd.DataFrame(
+                [
+                    [1, 1, 6, 6],
+                    [1, 2, 8, 8],
+                    [2, 2, 9, 9],
+                    [2, 3, 11, 11],
+                    [3, 5, 11, 11],
+                ],
+                columns=["x1", "x2", "y1", "y2"],
+            ),
+        ),
     ],
 )
 def test_work(df, method, columns, how, kwargs, expected):
