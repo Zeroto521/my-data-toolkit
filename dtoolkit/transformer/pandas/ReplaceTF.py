43af318c66460c1ffd398f47e0e15e5b1d9b9e8d
Zero <Zeroto521@gmail.com>
2022-06-02 14:54:10 +0800

    beautify a bit

--- dtoolkit/transformer/pandas/ReplaceTF.py
+++ dtoolkit/transformer/pandas/ReplaceTF.py
@@ -24,9 +24,13 @@ class ReplaceTF(DataFrameTF):
 
     >>> import pandas as pd
     >>> from dtoolkit.transformer import ReplaceTF
-    >>> df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
-    ...                    'B': [5, 6, 7, 8, 9],
-    ...                    'C': ['a', 'b', 'c', 'd', 'e']})
+    >>> df = pd.DataFrame(
+    ...     {
+    ...         'A': [0, 1, 2, 3, 4],
+    ...         'B': [5, 6, 7, 8, 9],
+    ...         'C': ['a', 'b', 'c', 'd', 'e'],
+    ...     },
+    ... )
     >>> tf = ReplaceTF(0, 5)
     >>> tf.transform(df)
     A  B  C
