7ad872a9d38ad3cdac65ea3c1e0ac2e2cd59aa0a
Zero <Zeroto521@gmail.com>
2022-06-25 17:49:56 +0800

    EHN: new `level` option for `to_set` (#586)
    * EHN: new `level` option for `to_set`
    
    * DOC: move them to notes
    
    * DOC: Add description for 'level'
    
    * Add examples for new option

--- dtoolkit/accessor/series/to_set.py
+++ dtoolkit/accessor/series/to_set.py
@@ -12,8 +12,6 @@ def to_set(s: pd.Series) -> set:
 
         set(s)
 
-    Different to :meth:`~pandas.Series.unique`, it returns :class:`~numpy.ndarray`.
-
     Returns
     -------
     set
@@ -22,6 +20,10 @@ def to_set(s: pd.Series) -> set:
     --------
     pandas.Series.unique
     dtoolkit.accessor.index.to_set
+
+    Notes
+    -----
+    Different to :meth:`~pandas.Index.unique`, it returns :class:`~pandas.Index`.
 
     Examples
     --------
