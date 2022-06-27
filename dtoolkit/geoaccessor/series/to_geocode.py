f63f4aee61f2d2d2a22192057a39e1d7069a4c21
Zero <Zeroto521@gmail.com>
2022-06-27 10:57:35 +0800

    Add kwargs for geocode

--- dtoolkit/geoaccessor/series/to_geocode.py
+++ dtoolkit/geoaccessor/series/to_geocode.py
@@ -5,12 +5,12 @@ from dtoolkit.accessor.register import register_series_method
 
 
 @register_series_method
-def to_geocode(s: pd.Series, drop: bool = False) -> gpd.GeoDataFrame:
+def to_geocode(s: pd.Series, drop: bool = False, **kwargs) -> gpd.GeoDataFrame:
     if s.name is None and not drop:
         raise ValueError(
             "to keep the original data requires setting the 'name' of "
             f"{s.__class__.__name__!r}",
         )
 
-    df = gpd.tools.geocode(s)
+    df = gpd.tools.geocode(s, **kwargs)
     return df if drop else pd.concat((s, df), axis=1)
