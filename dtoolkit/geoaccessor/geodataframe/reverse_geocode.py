9fab92834fe2ca078129c9fc3ba884a1e4dddd38
Zero <Zeroto521@gmail.com>
2022-06-27 17:38:31 +0800

    Finished basic description

--- dtoolkit/geoaccessor/geodataframe/reverse_geocode.py
+++ dtoolkit/geoaccessor/geodataframe/reverse_geocode.py
@@ -6,5 +6,29 @@ from dtoolkit.geoaccessor.register import register_geodataframe_method
 
 @register_geodataframe_method
 def reverse_geocode(df: gpd.GeoDataFrame, **kwargs) -> gpd.GeoDataFrame:
+    """
+    Reverse geocode Point type GeoDataFrame and get a GeoDataFrame of the resulting addresses.
+
+    Parameters
+    ----------
+    **kwargs
+        See the documentation for :func:`~geopandas.tools.geocode` for complete details
+        on the keyword arguments.
+
+    Returns
+    -------
+    GeoDataFrame
+
+    Raises
+    ------
+    ModuleNotFoundError
+        If don't have module named 'geopy'.
+
+    See Also
+    --------
+    dtoolkit.geoaccessor.series.geocode
+    dtoolkit.geoaccessor.dataframe.geocode
+    dtoolkit.geoaccessor.geoseries.reverse_geocode
+    """
 
     return gpd.tools.reverse_geocode(df.geometry, **kwargs)
--- dtoolkit/geoaccessor/geoseries/reverse_geocode.py
+++ dtoolkit/geoaccessor/geoseries/reverse_geocode.py
@@ -7,6 +7,30 @@ from dtoolkit.geoaccessor.register import register_geoseries_method
 
 @register_geoseries_method
 def reverse_geocode(s: gpd.GeoSeries, **kwargs) -> gpd.GeoDataFrame:
+    """
+    Reverse geocode Point type GeoSeries and get a GeoDataFrame of the resulting
+    addresses.
 
+    Parameters
+    ----------
+    **kwargs
+        See the documentation for :func:`~geopandas.tools.geocode` for complete details
+        on the keyword arguments.
+
+    Returns
+    -------
+    GeoDataFrame
+
+    Raises
+    ------
+    ModuleNotFoundError
+        If don't have module named 'geopy'.
+
+    See Also
+    --------
+    dtoolkit.geoaccessor.series.geocode
+    dtoolkit.geoaccessor.dataframe.geocode
+    dtoolkit.geoaccessor.geodataframe.reverse_geocode
+    """
 
     return gpd.tools.reverse_geocode(s.geometry, **kwargs)
