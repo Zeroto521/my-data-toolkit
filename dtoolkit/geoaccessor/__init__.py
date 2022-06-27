8f79b754f99562a68702c3d604edc73d64d3806b
Zero <Zeroto521@gmail.com>
2022-06-27 10:40:54 +0800

    Import geoaccessor.series all methods

--- dtoolkit/geoaccessor/__init__.py
+++ dtoolkit/geoaccessor/__init__.py
@@ -1,4 +1,5 @@
 from dtoolkit.geoaccessor import dataframe  # noqa
+from dtoolkit.geoaccessor import series  # noqa
 from dtoolkit.geoaccessor import geodataframe  # noqa
 from dtoolkit.geoaccessor import geoseries  # noqa
 from dtoolkit.geoaccessor.accessor import register_geodataframe_accessor  # noqa
