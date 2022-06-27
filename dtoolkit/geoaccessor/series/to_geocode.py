64fd41b5e7486099c7e32b26d9653721b7420919
Zero <Zeroto521@gmail.com>
2022-06-27 10:41:55 +0800

    Finished raising error

--- dtoolkit/geoaccessor/series/to_geocode.py
+++ dtoolkit/geoaccessor/series/to_geocode.py
@@ -7,7 +7,10 @@ from dtoolkit.accessor.register import register_series_method
 @register_series_method
 def to_geocode(s: pd.Series, drop: bool = False) -> gpd.GeoDataFrame:
     if s.name is None and not drop:
-        raise ValueError("")
+        raise ValueError(
+            "to keep the original data requires setting the 'name' of "
+            f"{s.__class__.__name__!r}",
+        )
 
     df = gpd.tools.geocode(s)
     return df if drop else pd.concat((s, df), axis=1)
