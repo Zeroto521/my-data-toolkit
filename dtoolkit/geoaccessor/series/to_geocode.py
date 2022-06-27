32d1a00b7f330d1f407a7d8879f5ac43a1dd02c4
Zero <Zeroto521@gmail.com>
2022-06-27 11:06:47 +0800

    Finished description

--- dtoolkit/geoaccessor/series/to_geocode.py
+++ dtoolkit/geoaccessor/series/to_geocode.py
@@ -6,6 +6,46 @@ from dtoolkit.accessor.register import register_series_method
 
 @register_series_method
 def to_geocode(s: pd.Series, drop: bool = False, **kwargs) -> gpd.GeoDataFrame:
+    """
+    Geocode string type Series and get a GeoDataFrame of the resulting points.
+
+    Parameters
+    ----------
+    drop : bool, default False
+        Don't contain the original data anymore.
+
+    **kwargs
+        See the documentation for :func:`~geopandas.tools.geocode` for complete details
+        on the keyword arguments.
+
+    Returns
+    -------
+    GeoDataFrame
+
+    Raises
+    ------
+    - ModuleNotFoundError
+        If don't have module named 'geopy'.
+
+    - ValueError
+        If 'drop' is True and the name of Series is empty.
+
+    Examples
+    --------
+    >>> import dtoolkit.geoaccessor
+    >>> import pandas as pd
+    >>> s = pd.Series(
+    ...     [
+    ...         "boston, ma",
+    ...         "1600 pennsylvania ave. washington, dc",
+    ...     ],
+    ... )
+    >>> s.to_geocode(drop=True)
+                         geometry                                            address
+    0  POINT (-71.06051 42.35543)               Boston, Massachusetts, United States
+    1  POINT (-77.03655 38.89770)  White House, 1600, Pennsylvania Avenue Northwe...
+    """
+
     if s.name is None and not drop:
         raise ValueError(
             "to keep the original data requires setting the 'name' of "
