ac9e308181a12cb155c28cdfb7af998684f5395b
Zero <Zeroto521@gmail.com>
2022-07-10 21:23:02 +0800

    init commit

---
+++ dtoolkit/geoaccessor/geoseries/topojson.py
@@ -0,0 +1,75 @@
+from __future__ import annotations
+
+from textwrap import dedent
+
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+
+from dtoolkit.geoaccessor.register import register_geoseries_method
+
+
+@register_geoseries_method
+@doc(
+    klass=":class:`~geopandas.GeoSeries`",
+    alias="s",
+    examples=dedent(
+        """
+    Examples
+    --------
+    >>> import dtoolkit.geoaccessor
+    >>> import geopandas as gpd
+    >>> from shapely.geometry import Point, LineString
+    >>> s = gpd.GeoSeries(
+    ...     [
+    ...         Point(122, 55),
+    ...         Point(100, 1),
+    ...         LineString([Point(122, 55), Point(100, 1)])
+    ...     ],
+    ...     crs="EPSG:4326",
+    ... )
+    >>> s
+    0    POINT (122.00000 55.00000)
+    1    POINT (100.00000  1.00000)
+    2    LINESTRING (122.00000 55.00000, 100.00000 1.00...
+    dtype: geometry
+    >>> s.geobuffer(100)
+    0    POLYGON ((122.00156 55.00001, 122.00156 54.999...
+    1    POLYGON ((100.00090 1.00000, 100.00089 0.99991...
+    2    POLYGON ((100.00088 0.99981, 100.00086 0.99972...
+    dtype: geometry
+    """,
+    ),
+)
+def geobuffer(
+    s: gpd.GeoSeries,
+    **kwargs,
+) -> gpd.GeoSeries:
+    """
+    Creates geographic buffers for {klass}.
+
+    Reprojects input features into the *UTM* projection, buffers them,
+    then reprojects back into the original geographic coordinates.
+
+    Parameters
+    ----------
+    distance : int, float, list-like of int or float, the unit is meter.
+        The radius of the buffer. If :obj:`~numpy.ndarray` or :obj:`~pandas.Series`
+        are used then it must have same length as the ``{alias}``.
+
+    Returns
+    -------
+    {klass}
+
+    See Also
+    --------
+    dtoolkit.geoaccessor.geoseries.geobuffer
+    dtoolkit.geoaccessor.geodataframe.geobuffer
+    geopandas.GeoSeries.buffer
+
+    {examples}
+    """
+
+    from topojson import Topology
+
+    
