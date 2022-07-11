eb779e94379b611ee1af3f40a6fd677d259151d1
Zero <Zeroto521@gmail.com>
2022-07-11 09:59:09 +0800

    replace noqa

--- dtoolkit/geoaccessor/geodataframe/__init__.py
+++ dtoolkit/geoaccessor/geodataframe/__init__.py
@@ -1,9 +1,9 @@
-from dtoolkit.geoaccessor.geodataframe.count_coordinates import (
+from dtoolkit.geoaccessor.geodataframe.count_coordinates import (  # noqa: F401
     count_coordinates,
-)  # noqa: F401
+)
 from dtoolkit.geoaccessor.geodataframe.drop_geometry import drop_geometry  # noqa: F401
 from dtoolkit.geoaccessor.geodataframe.geobuffer import geobuffer  # noqa: F401
-from dtoolkit.geoaccessor.geodataframe.get_coordinates import (
+from dtoolkit.geoaccessor.geodataframe.get_coordinates import (  # noqa: F401
     get_coordinates,
-)  # noqa: F401
+)
 from dtoolkit.geoaccessor.geodataframe.utm_crs import utm_crs  # noqa: F401
