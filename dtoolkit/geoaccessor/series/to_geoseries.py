ce1b72bd661ef1a3f56664f885ec019a4bdb30cb
Zero <Zeroto521@gmail.com>
2022-07-11 11:03:03 +0800

    refactor basic logic

--- dtoolkit/geoaccessor/series/to_geoseries.py
+++ dtoolkit/geoaccessor/series/to_geoseries.py
@@ -1,25 +1,28 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 import geopandas as gpd
 import pandas as pd
 from geopandas.base import is_geometry_type
+from pyproj import CRS
 
 from dtoolkit.accessor.register import register_series_method
-
-if TYPE_CHECKING:
-    from pyproj import CRS
 
 
 @register_series_method
 def to_geoseries(
     s: pd.Series,
     crs: CRS | str | int = None,
+    **kwargs,
 ) -> gpd.GeoSeries | pd.Series:
+
     if is_geometry_type(s):
-        s = gpd.GeoSeries(s)
-        if crs:
-            s = s.to_crs(crs) if s.crs else s.set_crs(crs)
+        crs = CRS.from_user_input(crs)
+        # Use `.copy` to avoid mutating the original Series.
+        s = gpd.GeoSeries(s.copy(), crs=crs, **kwargs)
+
+        # If `s` already has CRS, and it's not the same as `crs`.
+        # Then we need to transform it to the CRS the user set.
+        if s.crs != crs:
+            s = s.to_crs(crs)
 
     return s
