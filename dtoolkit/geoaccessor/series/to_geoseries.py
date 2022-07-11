f50e04b214490dc4cf23a50d1d1b6c22d7082335
Zero <Zeroto521@gmail.com>
2022-07-11 11:03:22 +0800

    Finished basic description

--- dtoolkit/geoaccessor/series/to_geoseries.py
+++ dtoolkit/geoaccessor/series/to_geoseries.py
@@ -14,6 +14,26 @@ def to_geoseries(
     crs: CRS | str | int = None,
     **kwargs,
 ) -> gpd.GeoSeries | pd.Series:
+    """
+    Transform an array of shapely scalars :class:`~pandas.Series` to
+    a :class:`~geopandas.GeoSeries`.
+
+    Parameters
+    ----------
+    crs : CRS, str, int, optional
+        Coordinate Reference System of the geometry objects. Can be anything
+        accepted by :meth:`~pyproj.crs.CRS.from_user_input`, such as an authority
+        string (eg "EPSG:4326" / 4326) or a WKT string.
+
+    **kwargs
+        See the documentation for :class:`~geopandas.GeoSeries` and  for complete
+        details on the keyword arguments.
+
+    Returns
+    -------
+    GeoSeries or Series
+        GeoSeries if the data is an array of shapely scalars.
+    """
 
     if is_geometry_type(s):
         crs = CRS.from_user_input(crs)
