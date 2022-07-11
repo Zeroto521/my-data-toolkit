af90a06dd2f2ab5459d298556485e447ad2ddcd5
Zero <Zeroto521@gmail.com>
2022-07-11 14:55:45 +0800

    Use `assert_frame_equal` replace `.equals`

--- test/transformer/pandas/test_QueryTF.py
+++ test/transformer/pandas/test_QueryTF.py
@@ -1,6 +1,8 @@
 from test.transformer.conftest import df_iris
 from test.transformer.conftest import feature_names
 
+from pandas.testing import assert_frame_equal
+
 from dtoolkit.transformer import QueryTF
 
 
@@ -8,14 +10,14 @@ def test_greater_symbol():
     tf = QueryTF(f"`{feature_names[0]}` > 0")
     result = tf.fit_transform(df_iris)
 
-    assert result.equals(df_iris)
+    assert_frame_equal(result, df_iris)
 
 
 def test_plus_symbol():
     tf = QueryTF(f"`{'`+`'.join(feature_names)}` < 100")
     result = tf.fit_transform(df_iris)
 
-    assert result.equals(df_iris)
+    assert_frame_equal(result, df_iris)
 
 
 def test_divide_symbol():
