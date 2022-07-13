6c42d317ff9aab58e887aca5efd31699ae43fc61
Zero <Zeroto521@gmail.com>
2022-07-13 09:21:14 +0800

    correct sphinx domain

--- dtoolkit/accessor/dataframe/cols.py
+++ dtoolkit/accessor/dataframe/cols.py
@@ -17,7 +17,7 @@ def cols(df: pd.DataFrame, /, to_list: bool = False) -> list[Hashable]:
     ----------
     to_list : bool, default False
         This option doesn't work, and it's used to fit
-        :method:`dtoolkit.accessor.series.cols` arguments.
+        :func:`dtoolkit.accessor.series.cols` arguments.
 
     Returns
     -------
