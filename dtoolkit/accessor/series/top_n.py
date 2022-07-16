7d6e224594cf0abc9246d29012346092bfe76960
Zero <Zeroto521@gmail.com>
2022-07-16 10:48:16 +0800

    replace raises, move series's to dataframe's

--- dtoolkit/accessor/series/top_n.py
+++ dtoolkit/accessor/series/top_n.py
@@ -43,11 +43,6 @@ def top_n(
     -------
     Series
 
-    Raises
-    ------
-    ValueError
-        If ``element`` isn't "both", "index" or "value".
-
     See Also
     --------
     dtoolkit.accessor.series.expand
