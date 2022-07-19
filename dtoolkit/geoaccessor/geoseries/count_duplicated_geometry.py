0975992955087d96058e2c278c269c6b62ab2418
Zero <Zeroto521@gmail.com>
2022-07-19 11:27:14 +0800

    complete documentation

--- dtoolkit/geoaccessor/geoseries/count_duplicated_geometry.py
+++ dtoolkit/geoaccessor/geoseries/count_duplicated_geometry.py
@@ -1,11 +1,63 @@
+from __future__ import annotations
+
+from textwrap import dedent
+
 import geopandas as gpd
 import pandas as pd
+from pandas.util._decorators import doc
 
 from dtoolkit.geoaccessor.register import register_geoseries_method
 
 
-@register_geodataframe_method
+@register_geoseries_method
+@doc(klass=":class:`~geopandas.GeoSeries`")
 def count_duplicated_geometry(s: gpd.GeoSeries, /, **kwargs) -> pd.Series:
+    """
+    Count the number of duplicated geometries in a {klass}.
+
+    Parameters
+    ----------
+    predicate / op : {{'intersects', 'crosses', 'overlaps', 'touches', 'covered_by', \
+'contains_properly', 'contains', 'within', 'covers'}}, default 'intersects'
+        The binary predicate is used to validate whether the geometries are duplicates
+        or not.
+        - geopandas version >= 0.10.0 : Please use ``predicate`.
+        - geopandas version < 0.10.0 : Please use ``op``.
+
+    Returns
+    -------
+    Series
+
+    See Also
+    --------
+    geopandas.sjoin
+
+    Examples
+    --------
+    >>> import dtoolkit.geoaccessor
+    >>> import geopandas as gpd
+    >>> df = (
+    ...     gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+    ...     .query('continent == "Africa"')
+    ... )
+    >>> df.head()
+         pop_est  ...                                           geometry
+    1   53950935  ...  POLYGON ((33.90371 -0.95000, 34.07262 -1.05982...
+    2     603253  ...  POLYGON ((-8.66559 27.65643, -8.66512 27.58948...
+    11  83301151  ...  POLYGON ((29.34000 -4.49998, 29.51999 -5.41998...
+    12   7531386  ...  POLYGON ((41.58513 -1.68325, 40.99300 -0.85829...
+    13  47615739  ...  POLYGON ((39.20222 -4.67677, 37.76690 -3.67712...
+    <BLANKLINE>
+    [5 rows x 6 columns]
+    >>> df.count_duplicated_geometry().head()
+    1      9
+    2      4
+    11    10
+    12     4
+    13     6
+    dtype: int64
+    """
+
     return (
         s.to_frame("geometry")
         .pipe(set_unique_index, drop=True)
