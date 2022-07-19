4c443183122291802f462933e77b94e74fcf8950
Zero <Zeroto521@gmail.com>
2022-07-19 11:28:58 +0800

    index method

--- dtoolkit/geoaccessor/geodataframe/__init__.py
+++ dtoolkit/geoaccessor/geodataframe/__init__.py
@@ -1,10 +1,10 @@
 from dtoolkit.geoaccessor.geodataframe.count_coordinates import (  # noqa: F401
     count_coordinates,
 )
-from dtoolkit.geoaccessor.geodataframe.drop_geometry import drop_geometry  # noqa: F401
-from dtoolkit.geoaccessor.geodataframe.duplicated_geometry import (  # noqa: F401
-    duplicated_geometry,
+from dtoolkit.geoaccessor.geodataframe.count_duplicated_geometry import (  # noqa: F401
+    count_duplicated_geometry,
 )
+from dtoolkit.geoaccessor.geodataframe.drop_geometry import drop_geometry  # noqa: F401
 from dtoolkit.geoaccessor.geodataframe.geobuffer import geobuffer  # noqa: F401
 from dtoolkit.geoaccessor.geodataframe.get_coordinates import (  # noqa: F401
     get_coordinates,
