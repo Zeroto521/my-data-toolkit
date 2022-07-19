73fbeb440f0947878e41942da6b96a27a5c65acf
Zero <Zeroto521@gmail.com>
2022-07-19 12:37:56 +0800

    use `count_duplicated_geometry` to simplify

--- dtoolkit/geoaccessor/geodataframe/duplicated_geometry.py
+++ dtoolkit/geoaccessor/geodataframe/duplicated_geometry.py
@@ -1,71 +1,12 @@
-from warnings import warn
-
 import geopandas as gpd
 import pandas as pd
+from pandas.util._decorators import doc
 
+from dtoolkit.geoaccessor.geoseries import duplicated_geometry as s_duplicated_geometry
 from dtoolkit.geoaccessor.register import register_geodataframe_method
 
 
 @register_geodataframe_method
+@doc(s_duplicated_geometry)
 def duplicated_geometry(df: gpd.GeoDataFrame, /, **kwargs) -> pd.Series:
-    return (
-        df.geometry.to_frame()
-        .pipe(set_unique_index, drop=True)
-        .pipe(self_sjoin, **kwargs)
-        .groupby_index()
-        .geometry.count()
-        .set_axis(df.index)
-        .rename(None)
-    )
-
-
-def set_unique_index(df: pd.DataFrame, /, **kwargs) -> pd.DataFrame:
-    """
-    Set unique index via ``.reset_index`` if ``df.index`` isn't unique.
-
-    Parameters
-    ----------
-    **kwargs
-        See the documentation for :meth:`~pandas.DataFrame.set_index` for complete
-        details on the keyword arguments.
-
-    Returns
-    -------
-    DataFrame
-
-    See Also
-    --------
-    pandas.DataFrame.reset_index
-    """
-
-    if not df.index.is_unique:
-        warn(f"The 'Index' of {type(df)} is not unique.")
-        return df.reset_index(**kwargs)
-
-    return df
-
-
-def self_sjoin(df: gpd.GeoDataFrame, /, **kwargs) -> gpd.GeoDataFrame:
-    """
-    Perform self-sjoin on a GeoDataFrame.
-
-    Parameters
-    ----------
-    **kwargs
-        See the documentation for :meth:`~geopandas.GeoDataFrame.sjoin` for complete
-        details on the keyword arguments except ``left_df`` and ``right_df``.
-
-    Returns
-    -------
-    GeoDataFrame
-
-    See Also
-    --------
-    geopandas.GeoDataFrame.sjoin
-
-    Notes
-    -----
-    ``left_df`` and ``right_df`` parameter are ``df`` and can't be overridden.
-    """
-
-    return gpd.sjoin(df, df, **kwargs)
+    return df.geometry.duplicated_geometry(**kwargs)
---
+++ dtoolkit/geoaccessor/geoseries/duplicated_geometry.py
@@ -0,0 +1,62 @@
+import geopandas as gpd
+import pandas as pd
+from pandas.util._decorators import doc
+
+from dtoolkit.geoaccessor.geoseries import count_duplicated_geometry  # noqa: f401
+from dtoolkit.geoaccessor.register import register_geoseries_method
+
+
+@register_geoseries_method
+@doc()
+def duplicated_geometry(s: gpd.GeoDataFrame, /, **kwargs) -> pd.Series:
+    """
+    Return boolean Series denoting duplicate geometries.
+
+    Parameters
+    ----------
+    Parameters
+    ----------
+    predicate / op : {{'intersects', 'crosses', 'overlaps', 'touches', 'covered_by', \
+'contains_properly', 'contains', 'within', 'covers'}}, default 'intersects'
+        The binary predicate is used to validate whether the geometries are duplicates
+        or not.
+        - geopandas version >= 0.10.0 : Please use ``predicate`.
+        - geopandas version < 0.10.0 : Please use ``op``.
+
+    Returns
+    -------
+    Series
+
+    See Also
+    --------
+    geopandas.sjoin
+    dtoolkit.geoaccessor.geoseries.duplicated_geometry
+    dtoolkit.geoaccessor.geodataframe.duplicated_geometry
+
+    Examples
+    --------
+    >>> import dtoolkit.geoaccessor
+    >>> import geopandas as gpd
+    >>> df = (
+    ...     gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+    ...     .query('continent == "Africa"')
+    ... )
+    >>> df.head()  # doctest: +SKIP
+         pop_est  ...                                           geometry
+    1   53950935  ...  POLYGON ((33.90371 -0.95000, 34.07262 -1.05982...
+    2     603253  ...  POLYGON ((-8.66559 27.65643, -8.66512 27.58948...
+    11  83301151  ...  POLYGON ((29.34000 -4.49998, 29.51999 -5.41998...
+    12   7531386  ...  POLYGON ((41.58513 -1.68325, 40.99300 -0.85829...
+    13  47615739  ...  POLYGON ((39.20222 -4.67677, 37.76690 -3.67712...
+    <BLANKLINE>
+    [5 rows x 6 columns]
+    >>> df.duplicated_geometry().head()
+    1     True
+    2     True
+    11    True
+    12    True
+    13    True
+    dtype: bool
+    """
+
+    return s.count_duplicated_geometry(**kwargs) >= 1
