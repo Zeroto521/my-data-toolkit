2b04120edbe7a2981137478b3b4d28ff0fd08154
Zero <Zeroto521@gmail.com>
2022-07-31 18:49:10 +0800

    EHN: New accessor for `Series`, `swap_index_values`

--- dtoolkit/accessor/series/__init__.py
+++ dtoolkit/accessor/series/__init__.py
@@ -11,6 +11,7 @@ from dtoolkit.accessor.series.jenks_bin import jenks_bin  # noqa: F401
 from dtoolkit.accessor.series.jenks_breaks import jenks_breaks  # noqa: F401
 from dtoolkit.accessor.series.len import len  # noqa: F401
 from dtoolkit.accessor.series.query import query  # noqa: F401
+from dtoolkit.accessor.series.swap_index_values import swap_index_values  # noqa: F401
 from dtoolkit.accessor.series.to_set import to_set  # noqa: F401
 from dtoolkit.accessor.series.top_n import top_n  # noqa: F401
 from dtoolkit.accessor.series.values_to_dict import values_to_dict  # noqa: F401
