8cf2cd869caca9150aee2474ee7e7d54909c31d4
Zero <Zeroto521@gmail.com>
2022-07-31 20:00:54 +0800

    Follow the apis of pandas.Series.duplicated

--- dtoolkit/geoaccessor/geoseries/duplicated_geometry.py
+++ dtoolkit/geoaccessor/geoseries/duplicated_geometry.py
@@ -1,9 +1,14 @@
+from typing import Literal
+
 import geopandas as gpd
 import pandas as pd
 from pandas.util._decorators import doc
 
+from dtoolkit.accessor.series import swap_index_values  # noqa: F401
 from dtoolkit.geoaccessor.geoseries import count_duplicated_geometry  # noqa: F401
 from dtoolkit.geoaccessor.geoseries.count_duplicated_geometry import BINARY_PREDICATE
+from dtoolkit.geoaccessor.geoseries.count_duplicated_geometry import self_sjoin
+from dtoolkit.geoaccessor.geoseries.count_duplicated_geometry import set_unique_index
 from dtoolkit.geoaccessor.register import register_geoseries_method
 
 
@@ -13,6 +18,7 @@ def duplicated_geometry(
     s: gpd.GeoDataFrame,
     /,
     predicate: BINARY_PREDICATE = "intersects",
+    keep: Literal["first", "last", False] = "first",
 ) -> pd.Series:
     """
     Return boolean Series denoting duplicate geometries.
@@ -23,6 +29,11 @@ def duplicated_geometry(
 'contains_properly', 'contains', 'within', 'covers'}}, default 'intersects'
         The binary predicate is used to validate whether the geometries are duplicates
         or not.
+
+    keep : {{'first', 'last', False}}, default 'first'
+        - ``first`` : Mark duplicates as ``True`` except for the first occurrence.
+        - ``last`` : Mark duplicates as ``True`` except for the last occurrence.
+        - False : Mark all duplicates as ``True``.
 
     Returns
     -------
@@ -60,4 +71,40 @@ def duplicated_geometry(
     dtype: bool
     """
 
-    return s.count_duplicated_geometry(predicate=predicate) >= 1
+    return s.iloc[
+        s.to_frame("geometry")
+        .pipe(set_unique_index, drop=True)
+        .pipe(self_sjoin, predicate=predicate)
+        .drop_geometry()
+        .to_series()
+        .pipe(lines_group, len(s))
+        .drop_duplicates(keep=keep)
+        .index
+    ]
+
+
+def lines_group(s: pd.Series, length: int) -> pd.Series:
+    groups = []
+    for i, v in s.items():
+        for group in groups:
+            if i in group or v in group:
+                group.update({i, v})
+                break
+        else:
+            groups.append({i, v})
+
+    for i in range(length):
+        for group in groups:
+            if i in group:
+                break
+        else:
+            groups.append({i})
+
+    return (
+        pd.Series(
+            groups,
+        )
+        .explode()
+        .pipe(swap_index_values)
+        .sort_index()
+    )
