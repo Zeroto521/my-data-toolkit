1c26e8e272c0805a0430803ad65b1aa42b7a54fd
Zero <Zeroto521@gmail.com>
2022-08-01 15:41:22 +0800

    EHN: New geoaccessor for geopandas, `drop_duplicates_geometry`

---
+++ dtoolkit/geoaccessor/geodataframe/drop_duplicates_geometry.py
@@ -0,0 +1,19 @@
+import geopandas as gpd
+import pandas as pd
+from pandas.util._decorators import doc
+
+from dtoolkit.geoaccessor.geoseries import (
+    drop_duplicates_geometry as s_drop_duplicates_geometry,
+)
+from dtoolkit.geoaccessor.geoseries.duplicated_geometry_groups import BINARY_PREDICATE
+from dtoolkit.geoaccessor.register import register_geodataframe_method
+
+
+@register_geodataframe_method
+@doc(s_drop_duplicates_geometry, klass="GeoDataFrame")
+def drop_duplicates_geometry(
+    df: gpd.GeoDataFrame,
+    /,
+    predicate: BINARY_PREDICATE = "intersects",
+) -> pd.Series:
+    return df.geometry.duplicated_geometry_groups(predicate=predicate)
---
+++ dtoolkit/geoaccessor/geoseries/drop_duplicates_geometry.py
@@ -0,0 +1,74 @@
+from typing import Literal
+
+import geopandas as gpd
+import pandas as pd
+from pandas.util._decorators import doc
+
+from dtoolkit.geoaccessor.geoseries.duplicated_geometry_groups import BINARY_PREDICATE
+from dtoolkit.geoaccessor.geoseries.duplicated_geometry import (  # noqa: F401
+    duplicated_geometry,
+)
+from dtoolkit.geoaccessor.register import register_geoseries_method
+
+
+@register_geoseries_method
+@doc(klass="GeoSeries")
+def drop_duplicates_geometry(
+    s: gpd.GeoSeries,
+    /,
+    predicate: BINARY_PREDICATE = "intersects",
+    keep: Literal["first", "last", False] = "first",
+) -> gpd.GeoSeries:
+    """
+    Remove duplicate geometry rows.
+
+    Parameters
+    ----------
+    predicate : {{'intersects', 'crosses', 'overlaps', 'touches', 'covered_by', \
+'contains_properly', 'contains', 'within', 'covers'}}, default 'intersects'
+        The binary predicate is used to validate whether the geometries are duplicates
+        or not.
+
+    keep : {{'first', 'last', False}}, default 'first'
+        - ``first`` : Mark duplicates as ``True`` except for the first occurrence.
+        - ``last`` : Mark duplicates as ``True`` except for the last occurrence.
+        - False : Mark all duplicates as ``True``.
+
+    Returns
+    -------
+    {klass}
+
+    See Also
+    --------
+    geopandas.sjoin
+    dtoolkit.geoaccessor.geoseries.duplicated_geometry
+    dtoolkit.geoaccessor.geoseries.drop_duplicates_geometry
+    dtoolkit.geoaccessor.geodataframe.duplicated_geometry
+    dtoolkit.geoaccessor.geodataframe.drop_duplicates_geometry
+
+    Examples
+    --------
+    >>> import dtoolkit.geoaccessor
+    >>> import geopandas as gpd
+    >>> from shapely.geometry import Polygon
+    >>> df = gpd.GeoDataFrame(
+    ...     geometry=[
+    ...         Polygon([(0,0), (1,0), (1,1), (0,1)]),
+    ...         Polygon([(1,1), (2,1), (2,2), (1,2)]),
+    ...         Polygon([(2,2), (3,2), (3,3), (2,3)]),
+    ...         Polygon([(2, 0), (3, 0), (3, 1)]),
+    ...     ],
+    ... )
+    >>> df
+                                                geometry
+    0  POLYGON ((0.00000 0.00000, 1.00000 0.00000, 1....
+    1  POLYGON ((1.00000 1.00000, 2.00000 1.00000, 2....
+    2  POLYGON ((2.00000 2.00000, 3.00000 2.00000, 3....
+    3  POLYGON ((2.00000 0.00000, 3.00000 0.00000, 3....
+    >>> df.drop_duplicates_geometry()
+                                                    geometry
+    0  POLYGON ((0.00000 0.00000, 1.00000 0.00000, 1....
+    3  POLYGON ((2.00000 0.00000, 3.00000 0.00000, 3....
+    """
+
+    return s[s.duplicated_geometry(predicate=predicate, keep=keep)]
