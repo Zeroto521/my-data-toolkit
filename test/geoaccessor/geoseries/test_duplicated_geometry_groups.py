b8ec6fda677e77b8ba5e1e7295f762e90ff332ef
Zero <Zeroto521@gmail.com>
2022-08-01 14:40:57 +0800

    Update test case

--- test/geoaccessor/geoseries/test_duplicated_geometry_groups.py
+++ test/geoaccessor/geoseries/test_duplicated_geometry_groups.py
@@ -1,20 +1,11 @@
-import geopandas as gpd
+import pandas as pd
 import pytest
-from shapely.geometry import Polygon
 
-from dtoolkit.geoaccessor.geodataframe import duplicated_geometry_groups  # noqa: F401
+from dtoolkit.geoaccessor.geoseries.duplicated_geometry_groups import set_unique_index
 
 
 def test_warning():
-    df = gpd.GeoDataFrame(
-        geometry=[
-            Polygon([(0, 0), (1, 0), (1, 1), (0, 1)]),
-            Polygon([(1, 1), (2, 1), (2, 2), (1, 2)]),
-            Polygon([(2, 2), (3, 2), (3, 3), (2, 3)]),
-            Polygon([(2, 0), (3, 0), (3, 1)]),
-        ],
-        index=[0, 0, 1, 2],
-    )
+    s = pd.Series(list("abcd"), index=[0, 0, 1, 2])
 
     with pytest.warns(UserWarning):
-        df.duplicated_geometry_groups()
+        set_unique_index(s)
