9bfbbe7a44fb0dcc290f988ed6d77580c3b50bb9
Zero <Zeroto521@gmail.com>
2022-08-01 15:22:43 +0800

    fit following feature

--- dtoolkit/geoaccessor/geoseries/duplicated_geometry.py
+++ dtoolkit/geoaccessor/geoseries/duplicated_geometry.py
@@ -4,11 +4,10 @@ import geopandas as gpd
 import pandas as pd
 from pandas.util._decorators import doc
 
-from dtoolkit.accessor.series import swap_index_values  # noqa: F401
-from dtoolkit.geoaccessor.geoseries import count_duplicated_geometry  # noqa: F401
-from dtoolkit.geoaccessor.geoseries.count_duplicated_geometry import BINARY_PREDICATE
-from dtoolkit.geoaccessor.geoseries.count_duplicated_geometry import self_sjoin
-from dtoolkit.geoaccessor.geoseries.count_duplicated_geometry import set_unique_index
+from dtoolkit.geoaccessor.geoseries.duplicated_geometry_groups import BINARY_PREDICATE
+from dtoolkit.geoaccessor.geoseries.duplicated_geometry_groups import (  # noqa: F401
+    duplicated_geometry_groups,
+)
 from dtoolkit.geoaccessor.register import register_geoseries_method
 
 
@@ -49,62 +48,27 @@ def duplicated_geometry(
     --------
     >>> import dtoolkit.geoaccessor
     >>> import geopandas as gpd
-    >>> df = (
-    ...     gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
-    ...     .query('continent == "Africa"')
+    >>> from shapely.geometry import Polygon
+    >>> df = gpd.GeoDataFrame(
+    ...     geometry=[
+    ...         Polygon([(0,0), (1,0), (1,1), (0,1)]),
+    ...         Polygon([(1,1), (2,1), (2,2), (1,2)]),
+    ...         Polygon([(2,2), (3,2), (3,3), (2,3)]),
+    ...         Polygon([(2, 0), (3, 0), (3, 1)]),
+    ...     ],
     ... )
-    >>> df.head()  # doctest: +SKIP
-         pop_est  ...                                           geometry
-    1   53950935  ...  POLYGON ((33.90371 -0.95000, 34.07262 -1.05982...
-    2     603253  ...  POLYGON ((-8.66559 27.65643, -8.66512 27.58948...
-    11  83301151  ...  POLYGON ((29.34000 -4.49998, 29.51999 -5.41998...
-    12   7531386  ...  POLYGON ((41.58513 -1.68325, 40.99300 -0.85829...
-    13  47615739  ...  POLYGON ((39.20222 -4.67677, 37.76690 -3.67712...
-    <BLANKLINE>
-    [5 rows x 6 columns]
-    >>> df.duplicated_geometry().head()
+    >>> df
+                                                geometry
+    0  POLYGON ((0.00000 0.00000, 1.00000 0.00000, 1....
+    1  POLYGON ((1.00000 1.00000, 2.00000 1.00000, 2....
+    2  POLYGON ((2.00000 2.00000, 3.00000 2.00000, 3....
+    3  POLYGON ((2.00000 0.00000, 3.00000 0.00000, 3....
+    >>> df.duplicated_geometry()
+    0    False
     1     True
     2     True
-    11    True
-    12    True
-    13    True
+    3    False
     dtype: bool
     """
 
-    return s.iloc[
-        s.to_frame("geometry")
-        .pipe(set_unique_index, drop=True)
-        .pipe(self_sjoin, predicate=predicate)
-        .drop_geometry()
-        .to_series()
-        .pipe(lines_group, len(s))
-        .drop_duplicates(keep=keep)
-        .index
-    ]
-
-
-def lines_group(s: pd.Series, length: int) -> pd.Series:
-    groups = []
-    for k, v in s.items():
-        for group in groups:
-            if k in group or v in group:
-                group.update({k, v})
-                break
-        else:
-            groups.append({k, v})
-
-    for i in range(length):
-        for group in groups:
-            if i in group:
-                break
-        else:
-            groups.append({i})
-
-    return (
-        pd.Series(
-            groups,
-        )
-        .explode()
-        .pipe(swap_index_values)
-        .sort_index()
-    )
+    return s.duplicated_geometry_groups(predicate=predicate).duplicated(keep=keep)
