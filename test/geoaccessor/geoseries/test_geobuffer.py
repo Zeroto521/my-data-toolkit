e42a607b28f3e6c10b5e0b303b320756baeac753
Zero <Zeroto521@gmail.com>
2022-08-08 17:35:21 +0800

    test empty point

--- test/geoaccessor/geoseries/test_geobuffer.py
+++ test/geoaccessor/geoseries/test_geobuffer.py
@@ -2,6 +2,7 @@ import geopandas as gpd
 import numpy as np
 import pandas as pd
 import pytest
+from shapely.geometry import Point
 
 from dtoolkit.geoaccessor.geoseries import geobuffer  # noqa: F401
 
@@ -44,7 +45,7 @@ def test_distance_length_is_different_to_data():
 
 
 def test_geometry_is_none():
-    s = gpd.GeoSeries([None], crs="epsg:4326")
+    s = gpd.GeoSeries([None, Point()], crs="epsg:4326")
     b = s.geobuffer(10)
 
     assert b[0] is None
