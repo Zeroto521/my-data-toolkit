c072afb75e13f9f003f0bd70f78a7222f5fa5958
Zero <Zeroto521@gmail.com>
2022-08-08 15:45:30 +0800

    Add example

--- dtoolkit/accessor/series/set_unique_index.py
+++ dtoolkit/accessor/series/set_unique_index.py
@@ -34,6 +34,30 @@ def set_unique_index(s: pd.Series, /, **kwargs) -> pd.Series:
     pandas.DataFrame.reset_index
     dtoolkit.accessor.series.set_unique_index
     dtoolkit.accessor.dataframe.set_unique_index
+
+    Examples
+    --------
+    >>> import dtoolkit.accessor
+    >>> import pandas as pd
+    >>> df = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]}, index=[0, 0, 1])
+    >>> df
+       a  b
+    0  1  4
+    0  2  5
+    1  3  6
+
+    If the index of the inputting is not unique, then it will be reset.
+    And give a warning like below.
+
+    ```python
+    UserWarning: The index of 'DataFrame' is not unique.
+    ```
+
+    >>> df.set_unique_index(drop=True)
+       a  b
+    0  1  4
+    1  2  5
+    2  3  6
     """
 
     if not s.index.is_unique:
