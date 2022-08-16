ad256a6590ede9cfd3c2d3a7879a3464aadd0fbc
Zero <Zeroto521@gmail.com>
2022-08-16 14:58:58 +0800

    wrap tesspy documentation with a decorator

--- dtoolkit/geoaccessor/geoseries/tessellation.py
+++ dtoolkit/geoaccessor/geoseries/tessellation.py
@@ -1,7 +1,26 @@
 import geopandas as gpd
-from pandas.util._decorators import doc
-
 from dtoolkit.geoaccessor.accessor import register_geoseries_accessor
+
+
+def tesspy_doc_decorator(func):
+    """
+    Wraps tesspy method docstring.
+
+    Can't directly use `pandas.util._decorators.doc` to wraps dtoolkit's method.
+    `tesspy` is a optional package for dtoolkit. We don't want to raise ImportError
+    while doing `import dtoolkit.geoaccessor` without installing tesspy.
+    """
+
+    try:
+        from pandas.util._decorators import doc
+        from tesspy import Tessellation as Tess
+
+        return doc(getattr(Tess, func.__name__))(func)
+
+    except ImportError:
+        ...
+
+    return func
 
 
 @register_geoseries_accessor("tess")
@@ -11,22 +30,22 @@ class Tessellation:
 
         self.tess = Tess(s.to_frame("geometry"))
 
-    @doc()
+    @tesspy_doc_decorator
     def squares(self, *args, **kwargs) -> gpd.GeoDataFrame:
         return self.tess.squares(*args, **kwargs)
 
-    @doc()
+    @tesspy_doc_decorator
     def hexagons(self, *args, **kwargs) -> gpd.GeoDataFrame:
         return self.tess.hexagons(*args, **kwargs)
 
-    @doc()
+    @tesspy_doc_decorator
     def adaptive_squares(self, *args, **kwargs) -> gpd.GeoDataFrame:
         return self.tess.adaptive_squares(*args, **kwargs)
 
-    @doc()
+    @tesspy_doc_decorator
     def voronoi(self, *args, **kwargs) -> gpd.GeoDataFrame:
         return self.tess.voronoi(*args, **kwargs)
 
-    @doc()
+    @tesspy_doc_decorator()
     def city_blocks(self, *args, **kwargs) -> gpd.GeoDataFrame:
         return self.tess.city_blocks(*args, **kwargs)
