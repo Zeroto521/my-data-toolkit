620376db48ef461664d7685c9608ff76c5d5d7b1
Zero <Zeroto521@gmail.com>
2022-08-23 15:07:44 +0800

    remove one link

--- dtoolkit/geoaccessor/geoseries/has_hole.py
+++ dtoolkit/geoaccessor/geoseries/has_hole.py
@@ -18,7 +18,6 @@ def has_hole(s: gpd.GeoSeries, /) -> pd.Series:
 
     See Also
     --------
-    geopandas.GeoSeries.interiors
     dtoolkit.geoaccessor.geoseries.has_hole
     dtoolkit.geoaccessor.geoseries.hole_counts
     dtoolkit.geoaccessor.geodataframe.has_hole
