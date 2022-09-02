dfeefb5f8e985352da37e507e4796a9211f21f35
Zero <Zeroto521@gmail.com>
2022-09-02 16:41:50 +0800

    update returning annotations

--- dtoolkit/geoaccessor/geoseries/filter_geometry.py
+++ dtoolkit/geoaccessor/geoseries/filter_geometry.py
@@ -37,7 +37,7 @@ def filter_geometry(
     predicate: BINARY_PREDICATE,
     complement: bool = False,
     **kwargs,
-) -> pd.Series:
+) -> gpd.GeoSeries:
     """
     Filter {klass} via the spatial relationship between {klass} and ``geometry``.
 
