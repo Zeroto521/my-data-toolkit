496272e894f88c0b38c8aca5c67cc368c3eeb578
40% <Zeroto521@gmail.com>
2022-09-05 09:11:02 +0800

    ENH: New geoaccessor to filter geometry via spatial relationship (#691)
    * New geoaccessor to filter geoemtry
    
    * index method
    
    * fix logic
    
    * update description
    
    * update example
    
    * Update description
    
    * BOT: auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update returning annotations
    
    * remove extra ```
    
    * descript `filter_geometry` is faster than boolean
    
    * New geoaccessor to filter geoemtry
    
    * index method
    
    * fix logic
    
    * update description
    
    * update example
    
    * Update description
    
    * BOT: auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update returning annotations
    
    * correct logic
    
    * lint codes
    
    * Create test_filter_geometry.py
    
    * BOT: auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add see-also part
    
    * adjust the sequences of code blocks
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ test/geoaccessor/geoseries/test_filter_geometry.py
@@ -0,0 +1,91 @@
+import geopandas as gpd
+import pytest
+from geopandas.testing import assert_geoseries_equal
+from shapely.geometry import box
+from shapely.geometry import LineString
+from shapely.geometry import Point
+from shapely.geometry import Polygon
+
+from dtoolkit.geoaccessor.geoseries import filter_geometry
+
+
+def test_error():
+    s = gpd.GeoSeries(
+        [
+            Polygon([(0, 0), (1, 1), (0, 1)]),
+            LineString([(0, 0), (0, 2)]),
+            LineString([(0, 0), (0, 1)]),
+            Point(0, 1),
+            Point(-1, -1),
+        ],
+    )
+
+    with pytest.raises(ValueError):
+        filter_geometry(s, box(0, 0, 2, 2), "error-predicate")
+
+
+@pytest.mark.parametrize(
+    "s, geometry, predicate, complement, expected",
+    [
+        (
+            gpd.GeoSeries(
+                [
+                    Polygon([(0, 0), (1, 1), (0, 1)]),
+                    LineString([(0, 0), (0, 2)]),
+                    LineString([(0, 0), (0, 1)]),
+                    Point(0, 1),
+                    Point(-1, -1),
+                ],
+            ),
+            box(0, 0, 2, 2),
+            "covered_by",
+            False,
+            gpd.GeoSeries(
+                [
+                    Polygon([(0, 0), (1, 1), (0, 1)]),
+                    LineString([(0, 0), (0, 2)]),
+                    LineString([(0, 0), (0, 1)]),
+                    Point(0, 1),
+                ],
+            ),
+        ),
+        (
+            gpd.GeoSeries(
+                [
+                    Polygon([(0, 0), (1, 1), (0, 1)]),
+                    LineString([(0, 0), (0, 2)]),
+                    LineString([(0, 0), (0, 1)]),
+                    Point(0, 1),
+                    Point(-1, -1),
+                ],
+            ),
+            box(0, 0, 2, 2),
+            "covered_by",
+            True,
+            gpd.GeoSeries([Point(-1, -1)], index=[4]),
+        ),
+        (
+            gpd.GeoSeries(
+                [
+                    Polygon([(0, 0), (1, 1), (0, 1)]),
+                    LineString([(0, 0), (0, 2)]),
+                    LineString([(0, 0), (0, 1)]),
+                    Point(0, 1),
+                    Point(-1, -1),
+                ],
+            ),
+            box(0, 0, 0, 0),
+            "covered_by",
+            False,
+            gpd.GeoSeries(),
+        ),
+    ],
+)
+def test_work(s, geometry, predicate, complement, expected):
+    result = s.filter_geometry(
+        geometry=geometry,
+        predicate=predicate,
+        complement=complement,
+    )
+
+    assert_geoseries_equal(result, expected)
