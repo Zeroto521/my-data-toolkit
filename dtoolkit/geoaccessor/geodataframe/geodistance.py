7b203b6b6d81ad0053645934a69b65935b60a3e4
Zero <Zeroto521@gmail.com>
2022-09-06 16:48:19 +0800

    correct name

--- dtoolkit/geoaccessor/geodataframe/geodistance.py
+++ dtoolkit/geoaccessor/geodataframe/geodistance.py
@@ -4,12 +4,12 @@ import geopandas as gpd
 import pandas as pd
 from pandas.util._decorators import doc
 
-from dtoolkit.geoaccessor.geoseries import geodistancey as s_geodistancey
+from dtoolkit.geoaccessor.geoseries import geodistance as s_geodistance
 from dtoolkit.geoaccessor.register import register_geodataframe_method
 
 
 @register_geodataframe_method
-@doc(s_geodistancey)
+@doc(s_geodistance)
 def geodistance(
     df: gpd.GeoDataFrame,
     /,
@@ -18,4 +18,4 @@ def geodistance(
     radius: float = 6371008.7714150598,
 ) -> pd.Series:
 
-    return s_distance(df.geometry, Other=other, align=align, radius=radius)
+    return s_geodistance(df.geometry, other=other, align=align, radius=radius)
