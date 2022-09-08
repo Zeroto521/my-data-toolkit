08073dd238a9096b4d50cb26fece3903e8481446
Zero <Zeroto521@gmail.com>
2022-09-08 10:21:45 +0800

    Create test_geodistance_matrix.py

---
+++ test/geoaccessor/geoseries/test_geodistance_matrix.py
@@ -0,0 +1,24 @@
+import pytest
+import geopandas as gpd
+from shapely.geometry import Point
+from dtoolkit.geoaccessor.geoseries import geodistance_matrix
+
+
+@pytest.mark.parametrize(
+    "s, other, error",
+    [
+        (
+            gpd.GeoSeries([Point(120, 30), Point(122, 55), Point(100, 1)]),
+            gpd.GeoSeries([Point(120, 30), Point(110, 40)], crs=4326),
+            ValueError,
+        ),
+        (
+            gpd.GeoSeries([Point(120, 30), Point(110, 40)], crs=4326),
+            gpd.GeoSeries([Point(120, 30), Point(122, 55), Point(100, 1)]),
+            ValueError,
+        ),
+    ],
+)
+def test_error(s, other, error):
+    with pytest.raises(error):
+        geodistance_matrix(s, other=other)
