83eb458ddc9158cd72234b57bf1ddd11ed09d623
Zero <Zeroto521@gmail.com>
2022-09-08 15:15:50 +0800

    test inputing geodataframe

--- test/geoaccessor/geoseries/test_geodistance_matrix.py
+++ test/geoaccessor/geoseries/test_geodistance_matrix.py
@@ -1,7 +1,8 @@
+import pandas as pd
 import geopandas as gpd
 import pytest
 from shapely.geometry import Point
-
+from pandas.testing import assert_frame_equal
 from dtoolkit.geoaccessor.geoseries import geodistance_matrix
 
 
@@ -23,3 +24,14 @@ from dtoolkit.geoaccessor.geoseries import geodistance_matrix
 def test_error(s, other, error):
     with pytest.raises(error):
         geodistance_matrix(s, other=other)
+
+
+def test_geodataframe():
+    s = gpd.GeoSeries([Point(122, 55), Point(100, 1)], crs=4326)
+    df = gpd.GeoSeries([Point(122, 55), Point(100, 1)], crs=4326).to_frame("geometry")
+
+    assert isinstance(df, gpd.GeoDataFrame)
+    result = s.geodistance_matrix(df) / 1e6
+    expected = pd.DataFrame([[0, 3.02679565], [3.02679565, 0]])
+
+    assert_frame_equal(result, expected)
