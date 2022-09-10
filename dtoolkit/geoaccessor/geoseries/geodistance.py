ff00fc921ce1f5458b6ceeddafca9cfeca139b90
Zero <Zeroto521@gmail.com>
2022-09-10 15:18:07 +0800

    update description

--- dtoolkit/geoaccessor/geoseries/geodistance.py
+++ dtoolkit/geoaccessor/geoseries/geodistance.py
@@ -20,12 +20,10 @@ def geodistance(
     radius: float = 6371008.7714150598,
 ) -> pd.Series:
     """
-    Returns a ``Series`` containing the `great-circle`__ distance to aligned other.
+    Returns a ``Series`` containing the `great-circle`__ distance to aligned other
+    via formula.
 
     __ https://en.wikipedia.org/wiki/Great-circle_distance
-
-    The algorithm uses the Vincenty formula which is more accurate than the Haversine
-    formula.
 
     .. math::
 
