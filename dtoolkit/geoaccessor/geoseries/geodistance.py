b8ebe935dd1546bdc730811b73bf3de2df47122f
Zero <Zeroto521@gmail.com>
2022-09-10 17:34:05 +0800

    update example

--- dtoolkit/geoaccessor/geoseries/geodistance.py
+++ dtoolkit/geoaccessor/geoseries/geodistance.py
@@ -170,8 +170,8 @@ def haversine(x1, y1, x2, y2) -> np.ndarray | float:
     --------
     >>> bsas = [-58.5166646, -34.83333]
     >>> paris = [2.53844117956, 49.0083899664]
-    >>> haversine(*bsas, *paris) * 63711  # multiply by Earth radius to get kilometers
-    110997.14575570964
+    >>> int(haversine(*bsas, *paris) * 63711)  # multiply by Earth radius to get kilometers
+    110997
     """
 
     x1, y1, x2, y2 = map(np.radians, (x1, y1, x2, y2))
