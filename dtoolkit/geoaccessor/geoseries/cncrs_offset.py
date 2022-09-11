correct formula

dtoolkit/geoaccessor/geoseries/cncrs_offset.py
@@ -229,7 +229,7 @@ def transform_x(s: gpd.GeoSeries | GeometryArray) -> pd.Series:
         300
         + x
         + 2 * y
-        + 0.1 * x * 2
+        + 0.1 * x ** 2
         + 0.1 * x * y
         + 0.1 * np.sqrt(np.fabs(x))
         + (20 * np.sin(x_dot_pi * 6) + 20 * np.sin(x_dot_pi * 2)) * 2 / 3