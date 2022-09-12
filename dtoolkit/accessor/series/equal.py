3899debf5f033368e2cbaf04c0790932c6e2e489
Zero <Zeroto521@gmail.com>
2022-09-12 10:25:33 +0800

    Simplify importing

--- dtoolkit/accessor/series/equal.py
+++ dtoolkit/accessor/series/equal.py
@@ -1,21 +1,15 @@
-from __future__ import annotations
-from typing import Any
-import pandas as pd
-import numpy as np
 from warnings import warn
+
+import numpy as np
+import pandas as pd
 from pandas.api.types import is_array_like
+
 from dtoolkit.accessor.register import register_series_method
 from dtoolkit.util._exception import find_stack_level
 
 
 @register_series_method
-def equal(
-    s: pd.Series,
-    /,
-    other: Any | list | np.ndarray | pd.Series,
-    align: bool = True,
-    **kwargs,
-) -> pd.Series:
+def equal(s: pd.Series, /, other, align: bool = True, **kwargs) -> pd.Series:
     if is_array_like(other):
         if align and isinstance(other, pd.Series) and not s.index.equals(other.index):
             warn("The indices are different.", stacklevel=find_stack_level())
