c434c0f9eaacc9395ad341de60dcd8bd5d1f97a1
Zero <Zeroto521@gmail.com>
2022-09-18 10:03:36 +0800

    support DataFrame

--- dtoolkit/accessor/dataframe/equal.py
+++ dtoolkit/accessor/dataframe/equal.py
@@ -19,14 +19,16 @@ def equal(
     **kwargs,
 ) -> pd.DataFrame:
     if is_array_like(other):
-        if align and isinstance(other, pd.Series) and not df.index.equals(other.index):
-            # FIXME: Wether check DataFrame's index.
-            # And use `.align` will change the values of `df`.
+        axis = df._get_axis_number(axis)
+        if (
+            align
+            and isinstance(other, (pd.Series, pd.DataFrame))
+            and not df.index.equals(other.index)
+        ):
             warn("The indices are different.", stacklevel=find_stack_level())
-            df, other = df.align(other)
+            df, other = df.align(other, axis=1 - axis)
 
         if len(shape_other := np.shape(other)) == 1:
-            axis = df._get_axis_number(axis)
             if shape_other[0] != df.shape[1 - axis]:
                 raise ValueError(
                     f"size of other ({shape_other[0]}) does not equal to df.shape"
