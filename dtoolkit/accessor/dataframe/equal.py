d9c3166454ebe9eb9483675d8f81f4f0b5a57d6a
Zero <Zeroto521@gmail.com>
2022-09-19 12:40:41 +0800

    Fix index/column comparing logic

--- dtoolkit/accessor/dataframe/equal.py
+++ dtoolkit/accessor/dataframe/equal.py
@@ -130,11 +130,20 @@ def equal(
 
     axis = df._get_axis_number(axis)
     if align and (
-        isinstance(other, pd.Series)
-        and not df.index.equals(other.index)
-        or isinstance(other, pd.DataFrame)
-        and not df.index.equals(other.index)
-        and not df.columns.equals(other.columns)
+        (
+            isinstance(other, pd.Series)
+            and (
+                axis == 0
+                and not df.columns.equals(other.index)
+                or axis == 1
+                and not df.index.equals(other.index)
+            )
+        )
+        or (
+            isinstance(other, pd.DataFrame)
+            and not df.index.equals(other.index)
+            and not df.columns.equals(other.columns)
+        )
     ):
         warn("the indices are different.", stacklevel=find_stack_level())
         df, other = df.align(
