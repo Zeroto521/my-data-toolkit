2c2765d924f87c0b246e8a0b5229d4d17ec16dca
Zero <Zeroto521@gmail.com>
2022-09-23 17:30:19 +0800

    ENH: New accessor for Series to convert datetime type

---
+++ dtoolkit/accessor/series/to_datetime.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+from dtoolkit.accessor.register import register_series_method
+
+
+@register_series_method
+def to_datetime(s: pd.Series, /, **kwargs) -> pd.Series:
+    """
+    Convert Series to datetime type.
+
+    A sugary syntax wraps::
+
+        pd.to_datetime(s, **kwargs)
+
+    Parameters
+    ----------
+    **kwargs
+        Keyword arguments to pass to :meth:`pandas.to_datetime`.
+
+    See Also
+    --------
+    pandas.to_datetime
+        The function that this method wraps.
+
+    janitor.functions.to_datetime
+        DataFrame version of this function. See `Janitor's documentation`__ for details.
+
+    __ https://pyjanitor-devs.github.io/pyjanitor/api/functions/#janitor.functions.to_datetime
+
+    Examples
+    --------
+    >>> import dtoolkit.accessor
+    >>> import pandas as pd
+    >>> s = pd.Series(['20200101', '20200202', '20200303'])
+    >>> s
+    0    20200101
+    1    20200202
+    2    20200303
+    dtype: object
+    >>> s.to_datetime(format='%Y%m%d')
+    0   2020-01-01
+    1   2020-02-02
+    2   2020-03-03
+    dtype: datetime64[ns]
+    """
+
+    return pd.to_datetime(s, **kwargs)
