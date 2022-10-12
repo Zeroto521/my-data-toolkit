ddcd4c60e89a1a266d7941d2ae29d3898f0f7d47
Zero <Zeroto521@gmail.com>
2022-10-12 12:38:15 +0800

    Add example for geoseries.xy_to_h3

--- dtoolkit/geoaccessor/geoseries/xy_to_h3.py
+++ dtoolkit/geoaccessor/geoseries/xy_to_h3.py
@@ -42,6 +42,25 @@ def xy_to_h3(
         https://h3geo.org/docs/api/indexing#latlngtocell
 
     dtoolkit.geoaccessor.geoseries.xy_to_h3
+
+    Examples
+    --------
+    >>> import dtoolkit.geoaccessor
+    >>> import pandas as pd
+    >>> s = pd.Series(
+    ...     [
+    ...         "POINT (122 55)",
+    ...         "POINT (100 1)",
+    ...     ],
+    ... ).from_wkt(crs=4326, drop=True)
+    >>> s
+    0    POINT (122.00000 55.00000)
+    1     POINT (100.00000 1.00000)
+    dtype: geometry
+    >>> s.xy_to_h3(8)
+    0    612845052823076863
+    1    614269156845420543
+    dtype: int64
     """
 
     # TODO: Use `latlon_to_h3` instead of `geo_to_h3`
@@ -54,6 +73,7 @@ def xy_to_h3(
     # TODO: Advices for h3-pandas
     # 1. use `import h3.api.numpy_int as h3` instead of `import h3`
     # 2. compat with h3-py 4
+    # 3. requires crs is 4326
 
     from h3.api.numpy_int import geo_to_h3
 
