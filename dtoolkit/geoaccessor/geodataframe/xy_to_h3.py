393c32ff1ddaa611fbb2714a416cf72146e0861d
Zero <Zeroto521@gmail.com>
2022-10-12 08:59:16 +0800

    Drop activate to turn geodataframe to dataframe

--- dtoolkit/geoaccessor/geodataframe/xy_to_h3.py
+++ dtoolkit/geoaccessor/geodataframe/xy_to_h3.py
@@ -3,6 +3,7 @@ from typing import Hashable
 import pandas as pd
 import geopandas as gpd
 
+from dtoolkit.geoaccessor.geodataframe.drop_geometry import drop_geometry
 from dtoolkit.geoaccessor.geoseries import xy_to_h3 as s_xy_to_h3
 from dtoolkit.geoaccessor.register import register_geodataframe_method
 
@@ -15,4 +16,4 @@ def xy_to_h3(
     column: Hashable = "h3",
 ) -> pd.DataFrame:
     h3 = s_xy_to_h3(df.geometry, resolution=resolution)
-    return df.assign(**{column: h3})
+    return drop_geometry(df).assign(**{column: h3})
