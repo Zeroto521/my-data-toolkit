d2d1ce2be0e9203546a279671d076918c7c97c6d
Zero <Zeroto521@gmail.com>
2022-10-13 11:38:59 +0800

    apply h3 method to series

--- dtoolkit/geoaccessor/series/h3.py
+++ dtoolkit/geoaccessor/series/h3.py
@@ -14,6 +14,74 @@ class H3:
         # TODO: raise error if s.dtype is not int64 or str
         self.s = s
 
+    @classmethod
+    def versions(cls) -> dict:
+        from h3 import versions
+
+        return versions()
+
+    @classmethod
+    def num_cells(cls, resolution: int) -> int:
+        from h3 import get_num_cells
+
+        return get_num_cells(resolution)
+
+    @classmethod
+    def res0_cells(cls) -> pd.Series:
+        from h3.api.numpy_int import get_res0_cells
+
+        return pd.Series(get_res0_cells())
+
+    @classmethod
+    def pentagons(cls, resolution: int) -> pd.Series:
+        from h3.api.numpy_int import get_pentagons
+
+        return pd.Series(get_pentagons(resolution))
+
+    @property
+    def resolution(self) -> pd.Series:
+        from h3.api.numpy_int import get_resolution
+
+        return self.s.apply(get_resolution)
+
+    @property
+    def is_valid(self) -> bool:
+        from h3.api.numpy_int import is_valid_cell
+
+        return self.s.apply(is_valid_cell)
+
+    @property
+    def is_res_class_III(self) -> pd.Series:
+        from h3.api.numpy_int import is_res_class_III
+
+        return self.s.apply(is_res_class_III)
+
+    @property
+    def is_pentagon(self) -> pd.Series:
+        from h3.api.numpy_int import is_pentagon
+
+        return self.s.apply(is_pentagon)
+
+    def edge(self, unit: Literal["km", "m", "rads"] = "km") -> pd.Series:
+        from h3.api.numpy_int import edge_length
+
+        return self.s.apply(edge_length, unit=unit)
+
+    def area(self, unit: Literal["km^2", "m^2", "rads^2"] = "km^2") -> pd.Series:
+        from h3.api.numpy_int import cell_area
+
+        return self.s.apply(cell_area, unit=unit)
+
+    def to_str(self) -> pd.Series:
+        from h3 import int_to_str
+
+        return self.s if self.s.dtype == "str" else self.s.apply(int_to_str)
+
+    def to_int(self) -> pd.Series:
+        from h3 import str_to_int
+
+        return self.s if self.s.dtype == "int64" else self.s.apply(str_to_int)
+
     def to_points(self, drop: bool = False) -> gpd.GeoSeries | gpd.GeoDataFrame:
         # TODO: Use `cell_to_latlng` instead of `h3_to_geo`
         # While h3-py release 4, `latlon_to_h3` is not available.
