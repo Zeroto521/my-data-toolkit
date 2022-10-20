200f8112ab1eb669d8dbc80da20ed2b6f17654ae
Zero <Zeroto521@gmail.com>
2022-10-20 21:53:56 +0800

    Add description

--- dtoolkit/accessor/index/filter.py
+++ dtoolkit/accessor/index/filter.py
@@ -18,6 +18,37 @@ def filter(
     regex: str | None = None,
     complement: bool = False,
 ) -> pd.Index:
+    """
+    Subset the Index according to the specified labels.
+
+    Parameters
+    ----------
+    items : list-like
+        Keep labels from axis which are in items.
+
+    like : str
+        Keep labels from axis for which "like in label == True".
+
+    regex : str (regular expression)
+        Keep labels from axis for which re.search(regex, label) == True.
+
+    complement : bool, default False
+        If True, return the complement of the result.
+
+    Returns
+    -------
+    Index
+
+    See Also
+    --------
+    pandas.Series.filter
+    pandas.DataFrame.filter
+
+    Notes
+    -----
+    The ``items``, ``like``, and ``regex`` parameters are enforced to be mutually
+    exclusive.
+    """
     if count_not_none(items, like, regex) > 1:
         raise TypeError(
             "Keyword arguments `items`, `like`, or `regex` are mutually exclusive.",
