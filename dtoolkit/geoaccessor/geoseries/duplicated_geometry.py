5f70fffccd520a979d9aafd33176866682f8fda0
Zero <Zeroto521@gmail.com>
2022-10-23 20:44:40 +0800

    update version

--- dtoolkit/geoaccessor/geoseries/duplicated_geometry.py
+++ dtoolkit/geoaccessor/geoseries/duplicated_geometry.py
@@ -16,7 +16,7 @@ from dtoolkit.util._decorator import warning
 @register_geoseries_method
 @warning(
     "`Geo(Series|DataFrame).duplicated_geometry`'s `predicate` default value is "
-    "changed from `'intersects'` to `None`. (Warning added DToolKit 0.0.18)"
+    "changed from `'intersects'` to `None`. (Warning added DToolKit 0.0.19)"
 )
 def duplicated_geometry(
     s: gpd.GeoDataFrame,
