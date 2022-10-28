7288110e98fe0075fd0a9bd621371e33cc3c7ffb
Zero <Zeroto521@gmail.com>
2022-10-28 14:32:11 +0800

    test via pytest not doctest

---
+++ test/geoaccessor/geodataframe/test_get_coordinates.py
@@ -0,0 +1,32 @@
+import pytest
+
+import pandas as pd
+import geopandas as gpd
+from pandas.testing import assert_series_equal
+
+from dtoolkit.geoaccessor.geodataframe import get_coordinates
+
+
+def test_work():
+    result = (
+        gpd.GeoSeries.from_wkt(
+            [
+                "POINT (0 0)",
+                "LINESTRING (2 2, 4 4)",
+                None,
+            ],
+        )
+        .to_frame("geometry")
+        .get_coordinates()
+    )
+
+    expected = pd.Series(
+        [
+            [[0.0, 0.0]],
+            [[2.0, 2.0], [4.0, 4.0]],
+            [],
+        ],
+        name="geometry",
+    )
+
+    assert_series_equal(result, expected)
