af0cef037527108dccd660f2e8301a1cd24eaeb9
40% <Zeroto521@gmail.com>
2022-11-05 19:49:01 +0800

    MAINT: Fix `DataFrame.append`'s FutureWarning (#576)
    * MAINT: Fix `DataFrame.append`'s FutureWarning
    
    * MAINT: Fix `DataFrame.append`'s FutureWarning
    
    * Compat pandas 1.4
    
    * BOT: auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * correct version
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- dtoolkit/transformer/_compat.py
+++ dtoolkit/transformer/_compat.py
@@ -1,5 +1,14 @@
 from packaging.version import Version
+from pandas import __version__ as pandas_version
 from sklearn import __version__ as sklearn_version
+
+
+# -----------------------------------------------------------------------------
+# pandas compat
+# -----------------------------------------------------------------------------
+
+PANDAS_GE_14 = Version(pandas_version) >= Version("1.4.0")
+
 
 # -----------------------------------------------------------------------------
 # scikit-learn compat
