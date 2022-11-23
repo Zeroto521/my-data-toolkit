b6af8dccb21b67bd01712ce0aaad083a4b1ca25b
Zero <Zeroto521@gmail.com>
2022-11-23 11:06:42 +0800

    add enable_checking and enable_cache options

--- dtoolkit/accessor/series/textdistance_matrix.py
+++ dtoolkit/accessor/series/textdistance_matrix.py
@@ -15,6 +15,8 @@ def textdistance_matrix(
     /,
     other: None | pd.Series = None,
     method: Callable = None,
+    enable_checking: bool = True,
+    enable_cache: bool = True,
 ) -> pd.DataFrame:
     """
     Returns a ``DataFrame`` containing the text distances matrix between in ``s``
@@ -28,6 +30,14 @@ def textdistance_matrix(
     method : Callable, default None
         The method to calculate the distance. If None, use
         `thefuzz.fuzz.ratio <https://github.com/seatgeek/thefuzz>`_.
+
+    enable_checking : bool, default True
+        If True, check the input. If the input is None, nan or empty string the result
+        will be 0.
+
+    enable_cache : bool, default True
+        If True, cache the result. The text distance algorithm is hrad to be vectorized,
+        and it's a time-consuming operation. Cache the result can speed up the process.
 
     Returns
     -------
@@ -70,7 +80,16 @@ def textdistance_matrix(
         raise TypeError(f"Expected Series(string), but got {other.dtype!r}.")
 
     return pd.concat(
-        (textdistance(s, o, method=method) for o in other),
+        (
+            textdistance(
+                s,
+                o,
+                method=method,
+                enable_checking=enable_checking,
+                enable_cache=enable_cache,
+            )
+            for o in other
+        ),
         axis=1,
         keys=other.index,
     )
