38a0343e67bb1b49a86683bf084aba77abfa2a9f
Zero <Zeroto521@gmail.com>
2022-11-23 15:29:43 +0800

    update example

--- dtoolkit/accessor/series/textdistance.py
+++ dtoolkit/accessor/series/textdistance.py
@@ -70,9 +70,9 @@ def textdistance(
     1    world
     dtype: object
     >>> s.textdistance("python")
-    0    36
-    1    18
-    dtype: int64
+    0    36.363636
+    1    18.181818
+    dtype: float64
     >>> s.textdistance(pd.Series(["hello", "python"]))
     0    36.363636
     1    18.181818
