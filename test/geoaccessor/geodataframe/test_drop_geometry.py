cf4b5ac0914fb26f5c968a4535fac88de4231556
Zero <Zeroto521@gmail.com>
2022-12-10 11:38:51 +0800

    Update test_drop_geometry.py

--- test/geoaccessor/geodataframe/test_drop_geometry.py
+++ test/geoaccessor/geodataframe/test_drop_geometry.py
@@ -2,7 +2,6 @@ import pandas as pd
 import pytest
 from pandas.testing import assert_frame_equal
 
-from dtoolkit._compat import PANDAS_GE_20
 from dtoolkit.geoaccessor.geodataframe import drop_geometry  # noqa: F401
 
 
