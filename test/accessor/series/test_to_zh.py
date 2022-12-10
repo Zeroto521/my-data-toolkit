28e2eaa5ac45b8369e376d9deb5597cf8b41ac0e
Zero <Zeroto521@gmail.com>
2022-12-10 16:57:59 +0800

    Update test_to_zh.py

--- test/accessor/series/test_to_zh.py
+++ test/accessor/series/test_to_zh.py
@@ -4,7 +4,15 @@ import pytest
 from dtoolkit.accessor.series.to_zh import to_zh
 
 
-@pytest.mark.parametrize("s, error", [pd.Series([1, 2]), TypeError])
+@pytest.mark.parametrize(
+    "s, error",
+    [
+        (
+            pd.Series([1, 2]),
+            TypeError,
+        ),
+    ],
+)
 def test_error(s, error):
     with pytest.raises(error):
         to_zh(s)
