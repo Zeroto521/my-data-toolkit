c9d89fb9afdd8b16e30fd9f36880468718979b1e
Zero <Zeroto521@gmail.com>
2022-12-11 14:47:43 +0800

    Create test_to_h3.py

---
+++ test/geoaccessor/geoseries/test_to_h3.py
@@ -0,0 +1,44 @@
+import geopandas as gpd
+import pandas as pd
+import pytest
+from shapely.geometry import Point
+from shapely.geometry import linestring
+
+from dtoolkit.geoaccessor.geoseries import to_h3
+
+
+pytest.importorskip("h3")
+
+
+@pytest.mark.parametrize(
+    "s, resolution, drop, rvname, error",
+    [
+        # crs != 4326
+        (
+            gpd.GeoSeries([Point(122, 55), Point(100, 1)]),
+            8,
+            True,
+            None,
+            ValueError,
+        ),
+        # s.name is None and name is None
+        (
+            gpd.GeoSeries([Point(122, 55), Point(100, 1)], crs=4326),
+            8,
+            False,
+            None,
+            ValueError,
+        ),
+        # geometry type is not Point or Polygon
+        (
+            gpd.GeoSeries([LineString([(122, 55), (100, 1)])], crs=4326),
+            8,
+            True,
+            None,
+            TypeError,
+        ),
+    ],
+)
+def test_error(s, resolution, drop, name, error):
+    with pytest.raises(error):
+        to_h3(s, resolution=resolution, drop=drop, name=name)
