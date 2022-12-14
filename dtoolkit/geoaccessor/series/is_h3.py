d975857b6dc7fcd5158286c3876f6419ed1a0a0d
40% <Zeroto521@gmail.com>
2022-12-14 23:17:34 +0800

    Add example section

--- dtoolkit/geoaccessor/series/is_h3.py
+++ dtoolkit/geoaccessor/series/is_h3.py
@@ -19,6 +19,31 @@ def is_h3(s: pd.Series, /) -> bool:
     See Also
     --------
     h3.h3_is_valid
+
+    Examples
+    --------
+    >>> import dtoolkit.geoaccessor
+    >>> import pandas as pd
+
+    Str type H3 cell index.
+
+    >>> s = pd.Series(['88143541bdfffff', '886528b2a3fffff'])
+    >>> s
+    0    88143541bdfffff
+    1    886528b2a3fffff
+    dtype: object
+    >>> s.is_h3()
+    True
+
+    Int type H3 cell index.
+
+    >>> s = pd.Series([612845052823076863, 614269156845420543])
+    >>> s
+    0    612845052823076863
+    1    614269156845420543
+    dtype: int64
+    >>> s.is_h3()
+    True
     """
     # TODO: Use `is_valid_cell` instead of `h3_is_valid`
     # While h3-py release 4, `is_valid_cell` is not available.
