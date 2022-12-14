62152246ebfaf6fd6be38f8dc3bca0af303fdfdc
40% <Zeroto521@gmail.com>
2022-12-14 23:17:23 +0800

    Support str type

--- dtoolkit/geoaccessor/series/is_h3.py
+++ dtoolkit/geoaccessor/series/is_h3.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+from pandas.api.types import is_string_dtype
+from pandas.api.types import is_int64_dtype
+
 from dtoolkit.accessor.register import register_series_method
 
 
@@ -23,6 +26,13 @@ def is_h3(s: pd.Series, /) -> bool:
     # requires h3 >= 4
     # from h3.api.numpy_int import is_valid_cell
     # requires h3 < 4
-    from h3.api.numpy_int import h3_is_valid
+    if is_int64_dtype(s):
+        is_valid = __import__("h3").api.numpy_int.h3_is_valid
+    elif is_string_dtype(s):
+        is_valid = __import__("h3").api.basic_str.h3_is_valid
+    else:
+        raise TypeError(
+            f"Expected Series(string) or Series(int64), but got {s.dtype!r}"
+        )
 
-    return s.apply(h3_is_valid).all()
+    return s.apply(is_valid).all()
