cc11784b1f9aaed637b09e16db035ab25e108f87
Zero <Zeroto521@gmail.com>
2022-12-18 21:31:44 +0800

    Revert "Move importing block to top"
    This reverts commit b1fb6f0e6daebd92b29d626cea21f7e907fda0a3.

--- dtoolkit/geoaccessor/geoseries/count_coordinates.py
+++ dtoolkit/geoaccessor/geoseries/count_coordinates.py
@@ -1,7 +1,6 @@
 import geopandas as gpd
 import pandas as pd
 from pandas.util._decorators import doc
-from shapely import count_coordinates
 
 from dtoolkit.geoaccessor.register import register_geoseries_method
 
@@ -52,5 +51,6 @@ def count_coordinates(s: gpd.GeoSeries, /) -> pd.Series:
     2    0
     Name: geometry, dtype: int64
     """
+    from shapely import count_coordinates
 
     return s.apply(count_coordinates)
