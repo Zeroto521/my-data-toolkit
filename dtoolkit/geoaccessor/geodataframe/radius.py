684bb2a94fbe5087383eb613d1f72dc12168088e
Zero <Zeroto521@gmail.com>
2022-12-18 18:28:20 +0800

    New geoaccessor `radius`

---
+++ dtoolkit/geoaccessor/geodataframe/radius.py
@@ -0,0 +1,12 @@
+import geopandas as gpd
+import pandas as pd
+from pandas.util._decorators import doc
+
+from dtoolkit.geoaccessor.geoseries import radius as s_radius
+from dtoolkit.geoaccessor.register import register_geodataframe_method
+
+
+@register_geodataframe_method
+@doc(s_radius)
+def radius(df: gpd.GeoDataFrame, /) -> pd.Series:
+    return s_radius(df.geometry)
