eb0c27fa5e700a15a908529788962bdf3fc40e7d
Zero <Zeroto521@gmail.com>
2022-12-18 20:20:33 +0800

    Compat pandas 2.x

--- test/geoaccessor/geodataframe/test_get_coordinates.py
+++ test/geoaccessor/geodataframe/test_get_coordinates.py
@@ -2,29 +2,27 @@ import geopandas as gpd
 import pandas as pd
 from pandas.testing import assert_series_equal
 
-from dtoolkit.geoaccessor.geodataframe import get_coordinates  # noqa: F401
+from dtoolkit.geoaccessor.geodataframe import get_coordinates
 
 
-def test_work():
-    result = (
-        gpd.GeoSeries.from_wkt(
-            [
-                "POINT (0 0)",
-                "LINESTRING (2 2, 4 4)",
-                None,
-            ],
-        )
-        .to_frame("geometry")
-        .get_coordinates()
-    )
-
-    expected = pd.Series(
-        [
-            [[0.0, 0.0]],
-            [[2.0, 2.0], [4.0, 4.0]],
-            [],
-        ],
-        name="geometry",
-    )
+@pytest.mark.parametrize(
+    "s, expected",
+    [
+        (
+            gpd.GeoSeries.from_wkt(["POINT (0 0)"]).to_frame("geometry"),
+            pd.Series([[[0.0, 0.0]]], name="geometry"),
+        ),
+        (
+            gpd.GeoSeries.from_wkt(["LINESTRING (2 2, 4 4)"]).to_frame("geometry"),
+            pd.Series([[[2.0, 2.0], [4.0, 4.0]]], name="geometry"),
+        ),
+        (
+            gpd.GeoSeries.from_wkt([None]).to_frame("geometry"),
+            pd.Series([[]], name="geometry"),
+        ),
+    ],
+)
+def test_work(s, expected):
+    result = get_coordinates(s)
 
     assert_series_equal(result, expected)
