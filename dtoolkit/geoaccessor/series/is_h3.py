4809621c93f809c3f4c1f6be325ab1d5f443e231
40% <Zeroto521@gmail.com>
2022-12-20 22:52:09 +0800

    Adapt the changing

--- dtoolkit/geoaccessor/series/is_h3.py
+++ dtoolkit/geoaccessor/series/is_h3.py
@@ -1,26 +1,23 @@
-from typing import Callable
-
 import pandas as pd
-from pandas.api.types import is_int64_dtype
-from pandas.api.types import is_string_dtype
 
 from dtoolkit.accessor.register import register_series_method
-from dtoolkit.geoaccessor.index.is_h3 import method_from_h3
+from dtoolkit.geoaccessor.index import is_h3 as i_is_h3
 
 
 @register_series_method
 def is_h3(s: pd.Series, /) -> bool:
     """
-    Validate whether the whole series is H3 cell index.
+    Validate whether the whole Series is H3 cell index.
 
     Returns
     -------
     bool
-        True if the whole series is H3 cell index else False.
+        True if the whole Series is H3 cell index else False.
 
     See Also
     --------
-    dtoolkit.geoaccessor.series.H3.is_valid
+    dtoolkit.geoaccessor.index.is_h3
+    dtoolkit.geoaccessor.series.h3.is_valid
 
     Raises
     ------
@@ -37,25 +34,23 @@ def is_h3(s: pd.Series, /) -> bool:
 
     String type H3 cell index.
 
-    >>> s = pd.Series(['88143541bdfffff', '886528b2a3fffff'])
+    >>> s = pd.Series(['a', 'b'], index=['88143541bdfffff', '886528b2a3fffff'])
     >>> s
-    0    88143541bdfffff
-    1    886528b2a3fffff
+    88143541bdfffff    a
+    886528b2a3fffff    b
     dtype: object
     >>> s.is_h3()
     True
 
     Int type H3 cell index.
 
-    >>> s = pd.Series([612845052823076863, 614269156845420543])
+    >>> s = pd.Series(['a', 'b'], index=[612845052823076863, 614269156845420543])
     >>> s
-    0    612845052823076863
-    1    614269156845420543
-    dtype: int64
+    612845052823076863    a
+    614269156845420543    b
+    dtype: object
     >>> s.is_h3()
     True
     """
 
-    # TODO: Use `is_valid_cell` instead of `h3_is_valid`
-    # While h3-py release 4, `is_valid_cell` is not available.
-    return s.apply(method_from_h3(s, "h3_is_valid")).all()
+    return i_is_h3(s.index)
