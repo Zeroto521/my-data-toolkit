0c3dcfb564585e8ad1e537c6770f90bf7f9c0090
40% <Zeroto521@gmail.com>
2022-12-20 19:51:08 +0800

    New accessor `len` for Index

--- dtoolkit/accessor/series/len.py
+++ dtoolkit/accessor/series/len.py
@@ -1,8 +1,6 @@
-from __future__ import annotations
-
 import pandas as pd
-from pandas.api.types import is_number
 
+from dtoolkit.accessor.index.len import length
 from dtoolkit.accessor.register import register_series_method
 
 
@@ -26,10 +24,14 @@ def len(s: pd.Series, /, number: int = 1, other: int = None) -> pd.Series:
     -------
     Series(int64)
 
+    See Also
+    --------
+    dtoolkit.accessor.index.len
+
     Notes
     -----
-    - To keep the Python naming style, so use this accessor via
-      ``Series.len`` rather than ``Series.lens``.
+    - To keep the Python naming style, so use this accessor via ``Series.len``
+      rather than ``Series.lens``.
 
     - Different to :meth:`pandas.Series.str.len`. It only returns
       :class:`collections.abc.Iterable` type length. Other type will return `NaN`.
@@ -74,13 +76,5 @@ def len(s: pd.Series, /, number: int = 1, other: int = None) -> pd.Series:
     dtype: int64
     """
 
-    return s.apply(_wrap_len, number=number, other=other)
+    return s.apply(length, number=number, other=other)
 
-
-def _wrap_len(x, number: int, other: int | None) -> int | None:
-    if hasattr(x, "__len__"):
-        return x.__len__()
-    elif is_number(x):
-        return number
-
-    return other
