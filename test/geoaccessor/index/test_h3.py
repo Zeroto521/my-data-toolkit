2cf3a2b1b6e43a82cec0814e9f570adfb23416cc
40% <Zeroto521@gmail.com>
2022-12-22 22:30:25 +0800

    test to_children

--- test/geoaccessor/index/test_h3.py
+++ test/geoaccessor/index/test_h3.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import pytest
+from pandas.testing import assert_index_equal
+from pandas._libs.reshape import explode
 
 from dtoolkit.geoaccessor.index import H3
 
@@ -55,3 +57,43 @@ def test_to_int_error(index, error):
 def test_to_str_error(index, error):
     with pytest.raises(error):
         H3(index).to_str()
+
+
+@pytest.mark.parametrize(
+    "index, expected",
+    [
+        (
+            pd.Index([612845052823076863]),
+            pd.Index(
+                [
+                    617348652448612351,
+                    617348652448874495,
+                    617348652449136639,
+                    617348652449398783,
+                    617348652449660927,
+                    617348652449923071,
+                    617348652450185215,
+                ],
+            ),
+        ),
+        (
+            pd.Index([614269156845420543]),
+            pd.Index(
+                [
+                    618772756470956031,
+                    618772756471218175,
+                    618772756471480319,
+                    618772756471742463,
+                    618772756472004607,
+                    618772756472266751,
+                    618772756472528895,
+                ],
+            ),
+        ),
+    ],
+)
+def test_to_children(index, expected):
+    result, _ = explode(H3(index).to_children().to_numpy())
+    result = pd.Index(result, dtype=index.dtype)
+
+    assert_index_equal(result, expected)
