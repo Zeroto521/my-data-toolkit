d03c447d7b19f9cbf6d3c5efb4870d5d884b7bd8
40% <Zeroto521@gmail.com>
2022-12-23 20:49:54 +0800

    Drop keyword argument `drop`

--- dtoolkit/geoaccessor/geoseries/xy.py
+++ dtoolkit/geoaccessor/geoseries/xy.py
@@ -5,7 +5,6 @@ from typing import Hashable
 import geopandas as gpd
 import pandas as pd
 
-from dtoolkit._typing import SeriesOrFrame
 from dtoolkit.geoaccessor.register import register_geoseries_method
 from dtoolkit.util._decorator import warning
 
@@ -22,14 +21,19 @@ from dtoolkit.util._decorator import warning
     "(Warning added DToolKit 0.0.20)",
     stacklevel=3,
 )
+@warning(
+    "The keyword argument 'drop' is deprecated, please use "
+    "'.drop_geometry' method instead. (Warning added DToolKit 0.0.20)",
+    category=DeprecationWarning,
+    stacklevel=3,
+)
 def xy(
     s: gpd.GeoSeries,
     /,
     reverse: bool = False,
     frame: bool = True,
-    drop: bool = True,
     name: Hashable | tuple[Hashable, Hashable] = ("x", "y"),
-) -> SeriesOrFrame | gpd.GeoDataFrame:
+) -> gpd.GeoDataFrame:
     """
     Return the x and y location of Point geometries in a GeoSeries.
 
@@ -46,6 +50,10 @@ def xy(
 
     drop : bool, default True
         If True, drop the original geometry column.
+
+        .. deprecated:: 0.0.20
+            If you want to drop geometry column, please use
+            :meth:`~dtoolkit.geoaccessor.geodataframe.drop_geometry` method instead.
 
     name : Hashable or a tuple of Hashable, default ('x', 'y')
         If ``frame=True``, the column names of the returned DataFrame,
@@ -65,14 +73,7 @@ def xy(
 
     Returns
     -------
-    Series, DataFrame, or GeoDataFrame
-        - If ``drop=Fasle``, return a GeoDataFrame.
-
-        - If ``drop=True`` and ``frame=True``, return a DataFrame with x and y two
-          columns.
-
-        - If ``drop=True`` and ``frame=False``, return a Series with tuple of
-          coordinate.
+    GeoDataFrame
 
     See Also
     --------
@@ -92,37 +93,29 @@ def xy(
     2    POINT (0.00000 3.00000)
     dtype: geometry
 
-    Get the x and y coordinates of each point as a tuple.
-
-    >>> s.xy(frame=False, name=None)
-    0    (0.0, 1.0)
-    1    (0.0, 2.0)
-    2    (0.0, 3.0)
-    dtype: object
-
-    Set ``reverse=True`` to return (y, x).
-
-    >>> s.xy(reverse=True, frame=False, name=None)
-    0    (1.0, 0.0)
-    1    (2.0, 0.0)
-    2    (3.0, 0.0)
-    dtype: object
-
-    Set ``frame=True`` to return a DataFrame with x and y columns.
+    Set ``frame=True`` to return a GeoDataFrame with x and y columns.
 
     >>> s.xy()
-         x    y
-    0  0.0  1.0
-    1  0.0  2.0
-    2  0.0  3.0
-
-    Keep the original geometry column.
-
-    >>> s.xy(drop=False)
          x    y                 geometry
     0  0.0  1.0  POINT (0.00000 1.00000)
     1  0.0  2.0  POINT (0.00000 2.00000)
     2  0.0  3.0  POINT (0.00000 3.00000)
+
+    Get the x and y coordinates of each point as a tuple.
+
+    >>> s.xy(frame=False, name="coord")
+            coord                 geometry
+    0  (0.0, 1.0)  POINT (0.00000 1.00000)
+    1  (0.0, 2.0)  POINT (0.00000 2.00000)
+    2  (0.0, 3.0)  POINT (0.00000 3.00000)
+
+    Set ``reverse=True`` to return (y, x).
+
+    >>> s.xy(reverse=True, frame=False, name="coord")
+            coord                 geometry
+    0  (1.0, 0.0)  POINT (0.00000 1.00000)
+    1  (2.0, 0.0)  POINT (0.00000 2.00000)
+    2  (3.0, 0.0)  POINT (0.00000 3.00000)
     """
 
     coords = pd.concat((s.x, s.y), axis=1)
@@ -133,4 +126,4 @@ def xy(
     if not frame:
         coords = coords.apply(tuple, axis=1).rename(name)
 
-    return coords if drop else coords.to_geoframe(s)
+    return coords.to_geoframe(s)
