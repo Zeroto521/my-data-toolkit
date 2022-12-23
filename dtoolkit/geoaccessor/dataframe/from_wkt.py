54cbe1b1b5c28659f26721686d2eb5c41b218242
40% <Zeroto521@gmail.com>
2022-12-23 13:17:18 +0800

    Drop keyword argument `drop`

--- dtoolkit/geoaccessor/dataframe/from_wkt.py
+++ dtoolkit/geoaccessor/dataframe/from_wkt.py
@@ -6,20 +6,26 @@ from typing import TYPE_CHECKING
 import geopandas as gpd
 import pandas as pd
 
-from dtoolkit.accessor.dataframe import drop_or_not  # noqa: F401
 from dtoolkit.accessor.register import register_dataframe_method
+from dtoolkit.geoaccessor.dataframe.to_geoframe import to_geoframe
+from dtoolkit.util._decorator import warning
 
 if TYPE_CHECKING:
     from pyproj import CRS
 
 
 @register_dataframe_method
+@warning(
+    "The keyword argument 'drop' is deprecated, please use "
+    "'.drop(columns=[...])' method instead. (Warning added DToolKit 0.0.20)",
+    category=DeprecationWarning,
+    stacklevel=3,
+)
 def from_wkt(
     df: pd.DataFrame,
     /,
     geometry: Hashable,
     crs: CRS | str | int = None,
-    drop: bool = False,
 ) -> gpd.GeoDataFrame:
     """
     Generate :obj:`~geopandas.GeoDataFrame` of geometries from 'WKT' column of
@@ -39,6 +45,10 @@ def from_wkt(
 
     drop : bool, default False
         Don't contain original 'WKT' column anymore.
+
+        .. deprecated:: 0.0.20
+            If you want to drop 'WkT' column, please use
+            ``.drop(columns=[...])`` method instead.
 
     Returns
     -------
@@ -78,20 +88,12 @@ def from_wkt(
     0  POINT (1 1)  POINT (1.00000 1.00000)
     1  POINT (2 2)  POINT (2.00000 2.00000)
     2  POINT (3 3)  POINT (3.00000 3.00000)
-
-    Drop original 'wkt' column.
-
-    >>> df.from_wkt("wkt", drop=True)
-                      geometry
-    0  POINT (1.00000 1.00000)
-    1  POINT (2.00000 2.00000)
-    2  POINT (3.00000 3.00000)
     """
 
     # Avoid mutating the original DataFrame.
     # https://github.com/geopandas/geopandas/issues/1179
-    return gpd.GeoDataFrame(
-        df.copy().drop_or_not(drop=drop, columns=geometry),
+    return to_geoframe(
+        df.copy(),
         geometry=gpd.GeoSeries.from_wkt(df[geometry]),
         crs=crs,
     )
