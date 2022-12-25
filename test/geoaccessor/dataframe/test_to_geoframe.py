839b0e498e14ae280e3ea0f757cc7b2697e625f7
40% <Zeroto521@gmail.com>
2022-12-25 13:08:11 +0800

    `to_geoframe` could replace old geoemtry

--- test/geoaccessor/dataframe/test_to_geoframe.py
+++ test/geoaccessor/dataframe/test_to_geoframe.py
@@ -1,5 +1,7 @@
+import geopandas as gpd
 import pandas as pd
 from pandas.testing import assert_frame_equal
+from shapely import Point
 
 from dtoolkit.geoaccessor.dataframe import to_geoframe  # noqa: F401
 
@@ -23,3 +25,22 @@ def test_original_dataframe_type():
     df.to_geoframe(geometry=df["geom"])
 
     assert_frame_equal(df, df_copy)
+
+
+def test_replace_old_geoemtry():
+    df = gpd.GeoDataFrame(
+        {
+            "label": ["a", "b"],
+            "geometry": [Point(100, 1), Point(122, 50)],
+        },
+        crs=4326,
+    ).to_geoframe(
+        geometry=gpd.GeoSeries(
+            [Point(0, 0), Point(1, 1)],
+            crs=3857,
+        ),
+    )
+
+    assert df.crs == 3857
+    assert df.geometry.x.tolist() == [0, 1]
+    assert df.geometry.y.tolist() == [0, 1]
