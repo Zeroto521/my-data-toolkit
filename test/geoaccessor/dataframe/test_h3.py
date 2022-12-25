047338bee0bc2746ef73c8699d1d8de203cd0e16
40% <Zeroto521@gmail.com>
2022-12-25 19:05:35 +0800

    test other methods

--- test/geoaccessor/dataframe/test_h3.py
+++ test/geoaccessor/dataframe/test_h3.py
@@ -81,6 +81,70 @@ pytest.importorskip("h3")
             gpd.GeoDataFrame,
             4326,
         ),
+        # test to_children
+        (
+            (
+                gpd.GeoDataFrame(
+                    {
+                        "label": ["a", "b"],
+                        "geometry": [Point(100, 1), Point(122, 55)],
+                    },
+                    crs=4326,
+                )
+                .to_h3(8, int_dtype=False)
+                .h3.to_children()
+            ),
+            gpd.GeoDataFrame,
+            4326,
+        ),
+        # test to_parent
+        (
+            (
+                gpd.GeoDataFrame(
+                    {
+                        "label": ["a", "b"],
+                        "geometry": [Point(100, 1), Point(122, 55)],
+                    },
+                    crs=4326,
+                )
+                .to_h3(8, int_dtype=False)
+                .h3.to_parent()
+            ),
+            gpd.GeoDataFrame,
+            4326,
+        ),
+        # test to_points
+        (
+            (
+                gpd.GeoDataFrame(
+                    {
+                        "label": ["a", "b"],
+                        "geometry": [Point(100, 1), Point(122, 55)],
+                    },
+                    crs=4326,
+                )
+                .to_h3(8, int_dtype=False)
+                .h3.to_points()
+            ),
+            gpd.GeoDataFrame,
+            4326,
+        ),
+        # test to_polygons
+        (
+            (
+                gpd.GeoDataFrame(
+                    {
+                        "label": ["a", "b"],
+                        "geometry": [Point(100, 1), Point(122, 55)],
+                    },
+                    crs=4326,
+                )
+                .to_h3(8, int_dtype=False)
+                .h3.to_polygons()
+            ),
+            gpd.GeoDataFrame,
+            4326,
+        ),
     ],
 )
 def test_geodataframe_h3_accessor_return(df, klass, crs):
