8b780844c31090088ab2e06ed42894eb076bbb5a
40% <Zeroto521@gmail.com>
2023-01-07 21:07:34 +0800

    test tol parameter

--- test/geoaccessor/geoseries/test_geocentroid.py
+++ test/geoaccessor/geoseries/test_geocentroid.py
@@ -23,3 +23,10 @@ from dtoolkit.geoaccessor.geoseries import geocentroid
 def test_error(s, error):
     with pytest.raises(error):
         geocentroid(s)
+
+
+def test_work():
+    # test `tol` parameter
+    result = gpd.GeoSeries([Point(100, 50), Point(120, 50)], crs=4326).geocentroid()
+
+    assert result.equals(Point(110, 50))
