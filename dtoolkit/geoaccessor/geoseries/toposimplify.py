813aa886fede73249cea788d69064d1792876f2b
40% <Zeroto521@gmail.com>
2023-01-07 17:22:54 +0800

    Lint codes

--- dtoolkit/geoaccessor/geoseries/toposimplify.py
+++ dtoolkit/geoaccessor/geoseries/toposimplify.py
@@ -107,7 +107,8 @@ def toposimplify(
             prevent_oversimplify=prevent_oversimplify,
         )
         # `to_gdf` return is a GeoDataFrame, require GeoSeries
-        .to_gdf(crs=s.crs).to_series()
+        .to_gdf(crs=s.crs)
+        .to_series()
         # To fix https://github.com/mattijn/topojson/issues/164
         .set_axis(s.index)
     )
