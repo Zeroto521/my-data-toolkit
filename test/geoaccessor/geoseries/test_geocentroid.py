b6347a07e3661dc3c55de0524833cefc4ac54ebb
40% <Zeroto521@gmail.com>
2023-01-08 12:56:01 +0800

    lint codes

--- test/geoaccessor/geoseries/test_geocentroid.py
+++ test/geoaccessor/geoseries/test_geocentroid.py
@@ -1,12 +1,10 @@
 import geopandas as gpd
-import pytest
 from shapely import Point
 
-from dtoolkit.geoaccessor.geoseries import geocentroid
+from dtoolkit.geoaccessor.geoseries import geocentroid  # noqa: F401
 
 
 def test_tol():
-    # test `tol` parameter
     result = gpd.GeoSeries([Point(100, 50), Point(120, 50)], crs=4326).geocentroid()
 
     assert result.equals(Point(110, 50))
