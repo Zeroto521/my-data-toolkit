90551b6f6f75afbb91fb26f64b237f7ac475caa5
Zero <Zeroto521@gmail.com>
2023-01-12 23:19:30 +0800

    Update test_geocentroid.py

--- test/geoaccessor/geoseries/test_geocentroid.py
+++ test/geoaccessor/geoseries/test_geocentroid.py
@@ -7,4 +7,4 @@ from dtoolkit.geoaccessor.geoseries import geocentroid
 def test_one_point():
     s = gpd.GeoSeries(Point(100, 32), crs=4326)
 
-    assert s.geocentroid() == Point(100, 32)
+    assert geocentroid(s) == Point(100, 32)
