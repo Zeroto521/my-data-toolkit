02994fd8010a178da512afefd08c7755980ff420
Zero <Zeroto521@gmail.com>
2023-05-13 18:00:00 +0800

    Create test_voronoi.py

---
+++ test/geoaccessor/geoseries/test_voronoi.py
@@ -0,0 +1,31 @@
+import geopandas as gpd
+import pytest
+from shapely import LineString, Point
+
+from dtoolkit.geoaccessor.geoseries import voronoi
+
+
+@pytest.mark.parametrize(
+    "s, error",
+    [
+        (
+            gpd.GeoSeries([Point(122, 55), Point(100, 1)]),
+            IndexError,
+        ),
+        (
+            gpd.GeoSeries([Point(122, 55)]),
+            IndexError,
+        ),
+        (
+            gpd.GeoSeries(),
+            IndexError,
+        ),
+        (
+            gpd.GeoSeries([LineString([(122, 55), (100, 1)])]),
+            TypeError,
+        ),
+    ],
+)
+def test_error(s, error):
+    with pytest.raises(error):
+        voronoi(s)
