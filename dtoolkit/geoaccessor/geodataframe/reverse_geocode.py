591851a9094f8e7adbff7272b551b25394fb4b21
Zero <Zeroto521@gmail.com>
2023-05-24 21:39:36 +0800

    Add full parameters

--- dtoolkit/geoaccessor/geodataframe/reverse_geocode.py
+++ dtoolkit/geoaccessor/geodataframe/reverse_geocode.py
@@ -1,3 +1,8 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+from typing import Hashable
+
 import geopandas as gpd
 import pandas as pd
 from pandas.util._decorators import doc
@@ -6,14 +11,35 @@ from dtoolkit.geoaccessor.geodataframe import drop_geometry
 from dtoolkit.geoaccessor.geoseries import reverse_geocode as s_reverse_geocode
 from dtoolkit.geoaccessor.register import register_geodataframe_method
 
+if TYPE_CHECKING:
+    import geopy.geocoders
+
 
 @register_geodataframe_method
 @doc(s_reverse_geocode, klass="GeoDataFrame")
-def reverse_geocode(df: gpd.GeoDataFrame, /, **kwargs) -> gpd.GeoDataFrame:
+def reverse_geocode(
+    df: gpd.GeoDataFrame,
+    /,
+    address: Hashable = "address",
+    provider: str | geopy.geocoder = "photon",
+    min_delay_seconds: float = 0,
+    max_retries: int = 2,
+    error_wait_seconds: float = 5,
+    **kwargs
+) -> gpd.GeoDataFrame:
     return pd.concat(
         (
             drop_geometry(df),
-            s_reverse_geocode(df.geometry, **kwargs),
+            s_reverse_geocode(
+                df.geometry,
+                provider=provider,
+                address=address,
+                min_delay_seconds=min_delay_seconds,
+                max_retries=max_retries,
+                error_wait_seconds=error_wait_seconds,
+                **kwargs
+            ),
         ),
         axis=1,
     ).to_geoframe()
+
