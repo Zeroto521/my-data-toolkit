1676e2b42b3bec63706c225f43a3f2260523e81d
40% <Zeroto521@gmail.com>
2024-02-08 17:02:37 +0800

    Require 1.4 version

--- dtoolkit/transformer/_compat.py
+++ dtoolkit/transformer/_compat.py
@@ -1,6 +1,5 @@
 from packaging.version import Version
 from pandas import __version__ as pandas_version
-from sklearn import __version__ as sklearn_version
 
 
 # -----------------------------------------------------------------------------
@@ -8,10 +7,3 @@ from sklearn import __version__ as sklearn_version
 # -----------------------------------------------------------------------------
 
 PANDAS_GE_14 = Version(pandas_version) >= Version("1.4.0")
-
-
-# -----------------------------------------------------------------------------
-# scikit-learn compat
-# -----------------------------------------------------------------------------
-
-SKLEARN_GE_14 = Version(sklearn_version) >= Version("1.4.0")
