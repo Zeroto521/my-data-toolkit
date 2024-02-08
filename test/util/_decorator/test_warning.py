c0753b778b3cd4759c9f80fb6d463a3fea383303
40% <Zeroto521@gmail.com>
2024-02-08 15:28:43 +0800

    Split to two testcases

--- test/util/_decorator/test_warning.py
+++ test/util/_decorator/test_warning.py
@@ -1,5 +1,3 @@
-from warnings import catch_warnings
-
 import pytest
 
 from dtoolkit.util._decorator import warning
@@ -9,9 +7,6 @@ from dtoolkit.util._decorator import warning
     "args, kwargs, message, exception",
     [
         ((), {}, "Nothing", None),
-        ((), {}, "Nothing", UserWarning),
-        ((), {}, "Nothing", FutureWarning),
-        ((), {}, "Nothing", DeprecationWarning),
         ((1, 2, 3), {}, "Nothing", None),
         ((), {"a": 1, "b": 2, "c": 3}, "Nothing", None),
         ((1, 2, 3), {"a": 1, "b": 2, "c": 3}, "Nothing", None),
@@ -22,7 +17,26 @@ def test_work(args, kwargs, message, exception):
     def func(*args, **kwargs):
         return dict(args=args, kwargs=kwargs)
 
-    with catch_warnings(exception, match=message):
+    result = func(*args, **kwargs)
+    assert isinstance(result, dict)
+    assert result["args"] == args
+    assert result["kwargs"] == kwargs
+
+
+@pytest.mark.parametrize(
+    "args, kwargs, message, exception",
+    [
+        ((), {}, "Nothing", UserWarning),
+        ((), {}, "Nothing", FutureWarning),
+        ((), {}, "Nothing", DeprecationWarning),
+    ],
+)
+def test_warning(args, kwargs, message, exception):
+    @warning(message, exception)
+    def func(*args, **kwargs):
+        return dict(args=args, kwargs=kwargs)
+
+    with pytest.warns(exception, match=message):
         result = func(*args, **kwargs)
 
     assert isinstance(result, dict)
