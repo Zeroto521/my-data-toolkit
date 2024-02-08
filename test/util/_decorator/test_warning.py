ade9825798cc21050546a3d15301533543285008
40% <Zeroto521@gmail.com>
2024-02-08 15:32:31 +0800

    Remove keyword argument 'match'

--- test/util/_decorator/test_warning.py
+++ test/util/_decorator/test_warning.py
@@ -36,7 +36,7 @@ def test_warning(args, kwargs, message, exception):
     def func(*args, **kwargs):
         return dict(args=args, kwargs=kwargs)
 
-    with pytest.warns(exception, match=message):
+    with pytest.warns(exception, message):
         result = func(*args, **kwargs)
 
     assert isinstance(result, dict)
