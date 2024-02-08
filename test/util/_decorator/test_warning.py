f2675ad117c8b0df8a06469a9aca72fac05aa8ed
40% <Zeroto521@gmail.com>
2024-02-08 15:14:15 +0800

    directly repalce `warns` with `catch_warnings`

--- test/util/_decorator/test_warning.py
+++ test/util/_decorator/test_warning.py
@@ -20,7 +20,7 @@ def test_work(args, kwargs, message, exception):
     def func(*args, **kwargs):
         return dict(args=args, kwargs=kwargs)
 
-    with pytest.warns(exception, match=message):
+    with pytest.catch_warnings(exception, match=message):
         result = func(*args, **kwargs)
 
     assert isinstance(result, dict)
