9c660d514e83a67978a0cae87f530ef90ea48ce5
40% <Zeroto521@gmail.com>
2024-02-21 17:40:25 +0800

    Compact sklearn 1.4

--- dtoolkit/pipeline.py
+++ dtoolkit/pipeline.py
@@ -123,20 +123,20 @@ class Pipeline(SKPipeline):
     def fit_transform(self, X, y=None, **params) -> np.ndarray | SeriesOrFrame:
         routed_params = self._check_method_params(method="fit_transform", props=params)
 
-        X = self._fit(transform_series_to_frame(X), y, routed_params)
-        X = transform_series_to_frame(X)  # transform fit's output to DataFrame
+        Xt = self._fit(transform_series_to_frame(X), y, routed_params)
+        Xt = transform_series_to_frame(Xt)  # transform fit's output to DataFrame
 
         last_step = self._final_estimator
         with _print_elapsed_time("Pipeline", self._log_message(len(self.steps) - 1)):
             if last_step == "passthrough":
-                return X
+                return Xt
 
             last_step_params = routed_params[self.steps[-1][0]]
             if hasattr(last_step, "fit_transform"):
-                Xt = last_step.fit_transform(X, y, **last_step_params["fit_transform"])
+                Xt = last_step.fit_transform(Xt, y, **last_step_params["fit_transform"])
             else:
                 Xt = last_step.fit(Xt, y, **last_step_params["fit"]).transform(
-                    X,
+                    Xt,
                     **last_step_params["transform"],
                 )
 
@@ -157,8 +157,11 @@ class Pipeline(SKPipeline):
 
         for _, name, transformer in reverse_iter:
             Xt = transform_array_to_frame(
-                transformer.inverse_transform(transform_series_to_frame(Xt)),
-                **routed_params[name].inverse_transform,
+                transformer.inverse_transform(
+                    transform_series_to_frame(Xt),
+                    **routed_params[name].inverse_transform,
+                ),
+                Xt,
             )
 
         return transform_frame_to_series(Xt)
