5c97c938bcebc50385e4f2e3131cea7207ce5fb4
40% <Zeroto521@gmail.com>
2025-06-24 14:08:53 +0800

    DEP: remove `toposimplify`
    use native function (https://geopandas.org/en/stable/docs/reference/api/geopandas.GeoSeries.simplify_coverage.html)

--- dtoolkit/geoaccessor/geoseries/__init__.py
+++ dtoolkit/geoaccessor/geoseries/__init__.py
@@ -24,5 +24,4 @@ from dtoolkit.geoaccessor.geoseries.select_geom_type import (  # noqa: F401
     select_geom_type,
 )
 from dtoolkit.geoaccessor.geoseries.to_h3 import to_h3  # noqa: F401
-from dtoolkit.geoaccessor.geoseries.toposimplify import toposimplify  # noqa: F401
 from dtoolkit.geoaccessor.geoseries.voronoi import voronoi  # noqa: F401
