782522d0b0667a387791b09a37eb7e7718856169
40% <Zeroto521@gmail.com>
2026-01-28 08:56:03 +0800

    Update test to use DataFrame.equals for comparison
    Replaces assert_frame_equal with DataFrame.equals in the test for drop_geometry, simplifying the assertion and improving compatibility.

--- test/geoaccessor/geodataframe/test_drop_geometry.py
+++ test/geoaccessor/geodataframe/test_drop_geometry.py
@@ -48,7 +48,4 @@ from dtoolkit.geoaccessor.geodataframe import drop_geometry  # noqa: F401
 def test_work(df, expected):
     result = df.drop_geometry()
 
-    # The return of `.drop(columns='geometry')` is a `GeoDataFrame`
-    # not a `DataFrame`in geopandas 0.9.0.
-    # So there can't use `.equals` to compare the result.
-    assert_frame_equal(result, expected, check_dtype=False)
+    assert expected.equals(result)
